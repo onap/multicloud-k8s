6c1c1c739b8494539fbe6f316f750e4cb748addb
Shashank Kumar Shankar <shashank.kumar.shankar@intel.com>
2018-08-20 15:50:50 -0700

    Seed code for k8s multicloud plugin
    This patch provides the initial seed code for the multicloud Kubernetes
    plugin and also provides the plugin feature to add new Kubernetes
    kinds.
    
    Change-Id: Ie5ee414656665070cde2834c4855ac2ebc179a9a
    Issue-ID: MULTICLOUD-301
    Signed-off-by: Shashank Kumar Shankar <shashank.kumar.shankar@intel.com>
    Signed-off-by: Victor Morales <victor.morales@intel.com>

---
+++ src/k8splugin/krd/krd.go
@@ -0,0 +1,44 @@
+/*
+Copyright 2018 Intel Corporation.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package krd
+
+import (
+	"errors"
+
+	pkgerrors "github.com/pkg/errors"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+// GetKubeClient loads the Kubernetes configuation values stored into the local configuration file
+var GetKubeClient = func(configPath string) (kubernetes.Clientset, error) {
+	var clientset *kubernetes.Clientset
+
+	if configPath == "" {
+		return *clientset, errors.New("config not passed and is not found in ~/.kube. ")
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", configPath)
+	if err != nil {
+		return kubernetes.Clientset{}, pkgerrors.Wrap(err, "setConfig: Build config from flags raised an error")
+	}
+
+	clientset, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		return *clientset, err
+	}
+
+	return *clientset, nil
+}

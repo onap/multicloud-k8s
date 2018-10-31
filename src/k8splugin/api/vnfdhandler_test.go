0617a6adde91213582d5046e7264435bf214f17e
Kiran Kamineni <kiran.k.kamineni@intel.com>
2018-10-31 16:24:32 -0700

    Add vnf definition APIs
    Adding APIs for POST, GET, LIST (implemented via GET)
    and DELETE commands on /v1/vnfd base for creating,
    getting, listing and deleting VNF Definitions.
    P2: Added unit tests for vnfdhandler.go
    P3: Add unit tests for serialize and deserialize
    P4: Integrating review comments
    P5: Added customizable mocking for vnfdhandler_test
    P6: Added customizablt mocking for vnfd_test
        Note that this will soon need to be updated once
        the db changes go through in patch 71090
    
    Issue-ID: MULTICLOUD-393
    Change-Id: Id509bed370ab3bdc572c6ead22324c1ee3dbf82d
    Signed-off-by: Kiran Kamineni <kiran.k.kamineni@intel.com>

---
+++ src/k8splugin/api/vnfdhandler_test.go
@@ -0,0 +1,336 @@
+/*
+ * Copyright 2018 Intel Corporation, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"k8splugin/vnfd"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	pkgerrors "github.com/pkg/errors"
+)
+
+//Creating an embedded interface via anonymous variable
+//This allows us to make mockDB satisfy the DatabaseConnection
+//interface even if we are not implementing all the methods in it
+type mockVNFDefinition struct {
+	vnfd.VNFDefinitionInterface
+	// Items and err will be used to customize each test
+	// via a localized instantiation of mockVNFDefinition
+	Items []vnfd.VNFDefinition
+	Err   error
+}
+
+func (m *mockVNFDefinition) Create(inp vnfd.VNFDefinition) (vnfd.VNFDefinition, error) {
+	if m.Err != nil {
+		return vnfd.VNFDefinition{}, m.Err
+	}
+
+	return m.Items[0], nil
+}
+
+func (m *mockVNFDefinition) List() ([]vnfd.VNFDefinition, error) {
+	if m.Err != nil {
+		return []vnfd.VNFDefinition{}, m.Err
+	}
+
+	return m.Items, nil
+}
+
+func (m *mockVNFDefinition) Get(vnfID string) (vnfd.VNFDefinition, error) {
+	if m.Err != nil {
+		return vnfd.VNFDefinition{}, m.Err
+	}
+
+	return m.Items[0], nil
+}
+
+func (m *mockVNFDefinition) Delete(vnfID string) error {
+	return m.Err
+}
+
+func TestVnfdCreateHandler(t *testing.T) {
+	testCases := []struct {
+		label        string
+		reader       io.Reader
+		expected     vnfd.VNFDefinition
+		expectedCode int
+		vnfdClient   *mockVNFDefinition
+	}{
+		{
+			label:        "Missing Body Failure",
+			expectedCode: http.StatusBadRequest,
+			vnfdClient:   &mockVNFDefinition{},
+		},
+		{
+			label:        "Create without UUID",
+			expectedCode: http.StatusCreated,
+			reader: bytes.NewBuffer([]byte(`{
+				"name":"testdomain",
+				"description":"test description",
+				"service-type":"firewall"
+				}`)),
+			expected: vnfd.VNFDefinition{
+				UUID:        "123e4567-e89b-12d3-a456-426655440000",
+				Name:        "testvnf",
+				Description: "test description",
+				ServiceType: "firewall",
+			},
+			vnfdClient: &mockVNFDefinition{
+				//Items that will be returned by the mocked Client
+				Items: []vnfd.VNFDefinition{
+					{
+						UUID:        "123e4567-e89b-12d3-a456-426655440000",
+						Name:        "testvnf",
+						Description: "test description",
+						ServiceType: "firewall",
+					},
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.label, func(t *testing.T) {
+			vh := vnfdHandler{vnfdClient: testCase.vnfdClient}
+			req, err := http.NewRequest("POST", "/v1/vnfd", testCase.reader)
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+			hr := http.HandlerFunc(vh.vnfdCreateHandler)
+			hr.ServeHTTP(rr, req)
+
+			//Check returned code
+			if rr.Code != testCase.expectedCode {
+				t.Fatalf("Expected %d; Got: %d", testCase.expectedCode, rr.Code)
+			}
+
+			//Check returned body only if statusCreated
+			if rr.Code == http.StatusCreated {
+				got := vnfd.VNFDefinition{}
+				json.NewDecoder(rr.Body).Decode(&got)
+
+				if reflect.DeepEqual(testCase.expected, got) == false {
+					t.Errorf("vnfdCreateHandler returned unexpected body: got %v;"+
+						" expected %v", got, testCase.expected)
+				}
+			}
+		})
+	}
+}
+
+func TestVnfdListHandler(t *testing.T) {
+
+	testCases := []struct {
+		label        string
+		expected     []vnfd.VNFDefinition
+		expectedCode int
+		vnfdClient   *mockVNFDefinition
+	}{
+		{
+			label:        "List VNF Definitions",
+			expectedCode: http.StatusOK,
+			expected: []vnfd.VNFDefinition{
+				{
+					UUID:        "123e4567-e89b-12d3-a456-426655440000",
+					Name:        "testvnf",
+					Description: "test description",
+					ServiceType: "firewall",
+				},
+				{
+					UUID:        "123e4567-e89b-12d3-a456-426655441111",
+					Name:        "testvnf2",
+					Description: "test description",
+					ServiceType: "dns",
+				},
+			},
+			vnfdClient: &mockVNFDefinition{
+				// list of definitions that will be returned by the mockclient
+				Items: []vnfd.VNFDefinition{
+					{
+						UUID:        "123e4567-e89b-12d3-a456-426655440000",
+						Name:        "testvnf",
+						Description: "test description",
+						ServiceType: "firewall",
+					},
+					{
+						UUID:        "123e4567-e89b-12d3-a456-426655441111",
+						Name:        "testvnf2",
+						Description: "test description",
+						ServiceType: "dns",
+					},
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.label, func(t *testing.T) {
+			vh := vnfdHandler{vnfdClient: testCase.vnfdClient}
+			req, err := http.NewRequest("GET", "/v1/vnfd", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+			hr := http.HandlerFunc(vh.vnfdListHandler)
+
+			hr.ServeHTTP(rr, req)
+			//Check returned code
+			if rr.Code != testCase.expectedCode {
+				t.Fatalf("Expected %d; Got: %d", testCase.expectedCode, rr.Code)
+			}
+
+			//Check returned body only if statusOK
+			if rr.Code == http.StatusOK {
+				got := []vnfd.VNFDefinition{}
+				json.NewDecoder(rr.Body).Decode(&got)
+
+				if reflect.DeepEqual(testCase.expected, got) == false {
+					t.Errorf("vnfdListHandler returned unexpected body: got %v;"+
+						" expected %v", got, testCase.expected)
+				}
+			}
+		})
+	}
+}
+
+func TestVnfdGetHandler(t *testing.T) {
+
+	testCases := []struct {
+		label        string
+		expected     vnfd.VNFDefinition
+		inpUUID      string
+		expectedCode int
+		vnfdClient   *mockVNFDefinition
+	}{
+		{
+			label:        "Get VNF Definition",
+			expectedCode: http.StatusOK,
+			expected: vnfd.VNFDefinition{
+				UUID:        "123e4567-e89b-12d3-a456-426655441111",
+				Name:        "testvnf2",
+				Description: "test description",
+				ServiceType: "dns",
+			},
+			inpUUID: "123e4567-e89b-12d3-a456-426655441111",
+			vnfdClient: &mockVNFDefinition{
+				// list of definitions that will be returned by the mockclient
+				Items: []vnfd.VNFDefinition{
+					{
+						UUID:        "123e4567-e89b-12d3-a456-426655441111",
+						Name:        "testvnf2",
+						Description: "test description",
+						ServiceType: "dns",
+					},
+				},
+			},
+		},
+		{
+			label:        "Get Non-Exiting VNF Definition",
+			expectedCode: http.StatusInternalServerError,
+			inpUUID:      "123e4567-e89b-12d3-a456-426655440000",
+			vnfdClient: &mockVNFDefinition{
+				// list of definitions that will be returned by the mockclient
+				Items: []vnfd.VNFDefinition{},
+				Err:   pkgerrors.New("Internal Error"),
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.label, func(t *testing.T) {
+			vh := vnfdHandler{vnfdClient: testCase.vnfdClient}
+			req, err := http.NewRequest("GET", "/v1/vnfd/"+testCase.inpUUID, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+			hr := http.HandlerFunc(vh.vnfdGetHandler)
+
+			hr.ServeHTTP(rr, req)
+			//Check returned code
+			if rr.Code != testCase.expectedCode {
+				t.Fatalf("Expected %d; Got: %d", testCase.expectedCode, rr.Code)
+			}
+
+			//Check returned body only if statusOK
+			if rr.Code == http.StatusOK {
+				got := vnfd.VNFDefinition{}
+				json.NewDecoder(rr.Body).Decode(&got)
+
+				if reflect.DeepEqual(testCase.expected, got) == false {
+					t.Errorf("vnfdListHandler returned unexpected body: got %v;"+
+						" expected %v", got, testCase.expected)
+				}
+			}
+		})
+	}
+}
+
+func TestVnfdDeleteHandler(t *testing.T) {
+
+	testCases := []struct {
+		label        string
+		inpUUID      string
+		expectedCode int
+		vnfdClient   *mockVNFDefinition
+	}{
+		{
+			label:        "Delete VNF Definition",
+			expectedCode: http.StatusNoContent,
+			inpUUID:      "123e4567-e89b-12d3-a456-426655441111",
+			vnfdClient:   &mockVNFDefinition{},
+		},
+		{
+			label:        "Delete Non-Exiting VNF Definition",
+			expectedCode: http.StatusInternalServerError,
+			inpUUID:      "123e4567-e89b-12d3-a456-426655440000",
+			vnfdClient: &mockVNFDefinition{
+				Err: pkgerrors.New("Internal Error"),
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.label, func(t *testing.T) {
+			vh := vnfdHandler{vnfdClient: testCase.vnfdClient}
+			req, err := http.NewRequest("GET", "/v1/vnfd/"+testCase.inpUUID, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr := httptest.NewRecorder()
+			hr := http.HandlerFunc(vh.vnfdDeleteHandler)
+
+			hr.ServeHTTP(rr, req)
+			//Check returned code
+			if rr.Code != testCase.expectedCode {
+				t.Fatalf("Expected %d; Got: %d", testCase.expectedCode, rr.Code)
+			}
+		})
+	}
+}
---
+++ src/k8splugin/vnfd/vnfd_test.go
@@ -0,0 +1,351 @@
+/*
+ * Copyright 2018 Intel Corporation, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package vnfd
+
+import (
+	"github.com/hashicorp/consul/api"
+	pkgerrors "github.com/pkg/errors"
+	"k8splugin/db"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+//Creating an embedded interface via anonymous variable
+//This allows us to make mockDB satisfy the DatabaseConnection
+//interface even if we are not implementing all the methods in it
+type mockDB struct {
+	db.DatabaseConnection
+	Items api.KVPairs
+	Err   error
+}
+
+func (m *mockDB) CreateEntry(key string, value string) error {
+	return m.Err
+}
+
+func (m *mockDB) ReadEntry(key string) (string, bool, error) {
+	if m.Err != nil {
+		return "", false, m.Err
+	}
+
+	for _, kvpair := range m.Items {
+		if kvpair.Key == key {
+			return string(kvpair.Value), true, nil
+		}
+	}
+
+	return "", false, nil
+}
+
+func (m *mockDB) DeleteEntry(key string) error {
+	return m.Err
+}
+
+func (m *mockDB) ReadAll(prefix string) ([]string, error) {
+	if m.Err != nil {
+		return []string{}, m.Err
+	}
+
+	var res []string
+
+	for _, keypair := range m.Items {
+		res = append(res, keypair.Key)
+	}
+
+	return res, nil
+}
+
+func TestCreateEntry(t *testing.T) {
+
+	testCases := []struct {
+		label         string
+		inp           VNFDefinition
+		expectedError string
+		mockdb        *mockDB
+	}{
+		{
+			label: "Create VNF Definition Entry in Database",
+			inp: VNFDefinition{
+				UUID:        "123e4567-e89b-12d3-a456-426655440000",
+				Name:        "testvnf",
+				Description: "testvnf",
+				ServiceType: "firewall",
+			},
+			expectedError: "",
+			mockdb:        &mockDB{},
+		},
+		{
+			label:         "Failed Creation of Entry in Database",
+			expectedError: "Error Creating Entry",
+			mockdb: &mockDB{
+				Err: pkgerrors.New("Error Creating Entry"),
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.label, func(t *testing.T) {
+			db.DBconn = testCase.mockdb
+
+			err := testCase.inp.createEntry("vnfd/")
+			if err != nil {
+				if testCase.expectedError == "" {
+					t.Fatalf("CreateEntry returned an unexpected error %s", err)
+				}
+				if strings.Contains(err.Error(), testCase.expectedError) == false {
+					t.Fatalf("CreateEntry returned an unexpected error %s", err)
+				}
+
+			}
+		})
+	}
+}
+
+func TestCreate(t *testing.T) {
+
+	testCases := []struct {
+		label         string
+		inp           VNFDefinition
+		expectedError string
+		mockdb        *mockDB
+		expected      VNFDefinition
+	}{
+		{
+			label: "Create VNF Definition",
+			inp: VNFDefinition{
+				UUID:        "123e4567-e89b-12d3-a456-426655440000",
+				Name:        "testvnf",
+				Description: "testvnf",
+				ServiceType: "firewall",
+			},
+			expected: VNFDefinition{
+				UUID:        "123e4567-e89b-12d3-a456-426655440000",
+				Name:        "testvnf",
+				Description: "testvnf",
+				ServiceType: "firewall",
+			},
+			expectedError: "",
+			mockdb:        &mockDB{},
+		},
+		{
+			label:         "Failed Create VNF Definition",
+			expectedError: "Error Creating Definition",
+			mockdb: &mockDB{
+				Err: pkgerrors.New("Error Creating Definition"),
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.label, func(t *testing.T) {
+			db.DBconn = testCase.mockdb
+			vimpl := GetVNFDClient()
+			got, err := vimpl.Create(testCase.inp)
+			if err != nil {
+				if testCase.expectedError == "" {
+					t.Fatalf("Create returned an unexpected error %s", err)
+				}
+				if strings.Contains(err.Error(), testCase.expectedError) == false {
+					t.Fatalf("Create returned an unexpected error %s", err)
+				}
+			} else {
+				if reflect.DeepEqual(testCase.expected, got) == false {
+					t.Errorf("Create VNF returned unexpected body: got %v;"+
+						" expected %v", got, testCase.expected)
+				}
+			}
+		})
+	}
+}
+
+func TestList(t *testing.T) {
+
+	testCases := []struct {
+		label         string
+		expectedError string
+		mockdb        *mockDB
+		expected      []VNFDefinition
+	}{
+		{
+			label: "List VNF Definition",
+			expected: []VNFDefinition{
+				{
+					UUID:        "123e4567-e89b-12d3-a456-426655440000",
+					Name:        "testvnf",
+					Description: "testvnf",
+					ServiceType: "firewall",
+				},
+				{
+					UUID:        "123e4567-e89b-12d3-a456-426655441111",
+					Name:        "testvnf2",
+					Description: "testvnf2",
+					ServiceType: "dns",
+				},
+			},
+			expectedError: "",
+			mockdb: &mockDB{
+				Items: api.KVPairs{
+					&api.KVPair{
+						Key: "vnfd/123e4567-e89b-12d3-a456-426655440000",
+						Value: []byte("{\"name\":\"testvnf\"," +
+							"\"description\":\"testvnf\"," +
+							"\"uuid\":\"123e4567-e89b-12d3-a456-426655440000\"," +
+							"\"service-type\":\"firewall\"}"),
+					},
+					&api.KVPair{
+						Key: "vnfd/123e4567-e89b-12d3-a456-426655441111",
+						Value: []byte("{\"name\":\"testvnf2\"," +
+							"\"description\":\"testvnf2\"," +
+							"\"uuid\":\"123e4567-e89b-12d3-a456-426655441111\"," +
+							"\"service-type\":\"dns\"}"),
+					},
+				},
+			},
+		},
+		{
+			label:         "List Error",
+			expectedError: "DB Error",
+			mockdb: &mockDB{
+				Err: pkgerrors.New("DB Error"),
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.label, func(t *testing.T) {
+			db.DBconn = testCase.mockdb
+			vimpl := GetVNFDClient()
+			got, err := vimpl.List()
+			if err != nil {
+				if testCase.expectedError == "" {
+					t.Fatalf("List returned an unexpected error %s", err)
+				}
+				if strings.Contains(err.Error(), testCase.expectedError) == false {
+					t.Fatalf("List returned an unexpected error %s", err)
+				}
+			} else {
+				if reflect.DeepEqual(testCase.expected, got) == false {
+					t.Errorf("List VNF returned unexpected body: got %v;"+
+						" expected %v", got, testCase.expected)
+				}
+			}
+		})
+	}
+}
+
+func TestGet(t *testing.T) {
+
+	testCases := []struct {
+		label         string
+		expectedError string
+		mockdb        *mockDB
+		inp           string
+		expected      VNFDefinition
+	}{
+		{
+			label: "Get VNF Definition",
+			inp:   "123e4567-e89b-12d3-a456-426655440000",
+			expected: VNFDefinition{
+				UUID:        "123e4567-e89b-12d3-a456-426655440000",
+				Name:        "testvnf",
+				Description: "testvnf",
+				ServiceType: "firewall",
+			},
+			expectedError: "",
+			mockdb: &mockDB{
+				Items: api.KVPairs{
+					&api.KVPair{
+						Key: "vnfd/123e4567-e89b-12d3-a456-426655440000",
+						Value: []byte("{\"name\":\"testvnf\"," +
+							"\"description\":\"testvnf\"," +
+							"\"uuid\":\"123e4567-e89b-12d3-a456-426655440000\"," +
+							"\"service-type\":\"firewall\"}"),
+					},
+				},
+			},
+		},
+		{
+			label:         "Get Error",
+			expectedError: "DB Error",
+			mockdb: &mockDB{
+				Err: pkgerrors.New("DB Error"),
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.label, func(t *testing.T) {
+			db.DBconn = testCase.mockdb
+			vimpl := GetVNFDClient()
+			got, err := vimpl.Get(testCase.inp)
+			if err != nil {
+				if testCase.expectedError == "" {
+					t.Fatalf("Get returned an unexpected error %s", err)
+				}
+				if strings.Contains(err.Error(), testCase.expectedError) == false {
+					t.Fatalf("Get returned an unexpected error %s", err)
+				}
+			} else {
+				if reflect.DeepEqual(testCase.expected, got) == false {
+					t.Errorf("Get VNF returned unexpected body: got %v;"+
+						" expected %v", got, testCase.expected)
+				}
+			}
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+
+	testCases := []struct {
+		label         string
+		inp           string
+		expectedError string
+		mockdb        *mockDB
+		expected      []VNFDefinition
+	}{
+		{
+			label:  "Delete VNF Definition",
+			inp:    "123e4567-e89b-12d3-a456-426655440000",
+			mockdb: &mockDB{},
+		},
+		{
+			label:         "Delete Error",
+			expectedError: "DB Error",
+			mockdb: &mockDB{
+				Err: pkgerrors.New("DB Error"),
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.label, func(t *testing.T) {
+			db.DBconn = testCase.mockdb
+			vimpl := GetVNFDClient()
+			err := vimpl.Delete(testCase.inp)
+			if err != nil {
+				if testCase.expectedError == "" {
+					t.Fatalf("Delete returned an unexpected error %s", err)
+				}
+				if strings.Contains(err.Error(), testCase.expectedError) == false {
+					t.Fatalf("Delete returned an unexpected error %s", err)
+				}
+			}
+		})
+	}
+}

de765521787c039cb53c4171b1c8e90116bcb109
Kiran Kamineni <kiran.k.kamineni@intel.com>
2018-12-10 13:34:16 -0800

    Migrating from consul to mongodb for backend
    Migrating to mongodb from consul.
    This requires a little bit of hierarchy management
    and data management.
    We are no longer converting structs to json encoded
    strings. The underlying db supports structs without any modifications.
    Also, since Mongo has the concept of collections, each submodule can
    use its own collection for storage as needed.
    Definition uses a collection called rbdef right now.
    
    Issue-ID: MULTICLOUD-426
    Change-Id: I42d239b324025fc4ef4e561790aceeff794001ef
    Signed-off-by: Kiran Kamineni <kiran.k.kamineni@intel.com>

--- src/k8splugin/db/store_test.go
+++ src/k8splugin/db/store_test.go
@@ -23,9 +23,9 @@ import (
 
 func TestCreateDBClient(t *testing.T) {
 	t.Run("Successfully create DB client", func(t *testing.T) {
-		expected := &ConsulStore{}
+		expected := &MongoStore{}
 
-		err := CreateDBClient("consul")
+		err := CreateDBClient("mongo")
 		if err != nil {
 			t.Fatalf("CreateDBClient returned an error (%s)", err)
 		}
@@ -42,82 +42,4 @@ func TestCreateDBClient(t *testing.T) {
 			t.Fatalf("CreateDBClient method returned an error (%s)", err)
 		}
 	})
-}
-
-func TestSerialize(t *testing.T) {
-
-	inp := map[string]interface{}{
-		"UUID":   "123e4567-e89b-12d3-a456-426655440000",
-		"Data":   "sdaijsdiodalkfjsdlagf",
-		"Number": 23,
-		"Float":  34.4,
-		"Map": map[string]interface{}{
-			"m1": "m1",
-			"m2": 2,
-			"m3": 3.0,
-		},
-	}
-
-	got, err := Serialize(inp)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := "{\"Data\":\"sdaijsdiodalkfjsdlagf\"," +
-		"\"Float\":34.4,\"Map\":{\"m1\":\"m1\",\"m2\":2,\"m3\":3}," +
-		"\"Number\":23,\"UUID\":\"123e4567-e89b-12d3-a456-426655440000\"}"
-
-	if expected != got {
-		t.Errorf("Serialize returned unexpected string: %s;"+
-			" expected %sv", got, expected)
-	}
-}
-
-func TestDeSerialize(t *testing.T) {
-	testCases := []struct {
-		label    string
-		input    string
-		expected map[string]interface{}
-		errMsg   string
-	}{
-		{
-			label: "Sucessful deserialize entry",
-			input: "{\"Data\":\"sdaijsdiodalkfjsdlagf\"," +
-				"\"Float\":34.4,\"Map\":{\"m1\":\"m1\",\"m3\":3}," +
-				"\"UUID\":\"123e4567-e89b-12d3-a456-426655440000\"}",
-			expected: map[string]interface{}{
-				"UUID":  "123e4567-e89b-12d3-a456-426655440000",
-				"Data":  "sdaijsdiodalkfjsdlagf",
-				"Float": 34.4,
-				"Map": map[string]interface{}{
-					"m1": "m1",
-					"m3": 3.0,
-				},
-			},
-		},
-		{
-			label:  "Fail to deserialize invalid entry",
-			input:  "{invalid}",
-			errMsg: "Error deSerializing {invalid}: invalid character 'i' looking for beginning of object key string",
-		},
-	}
-	for _, testCase := range testCases {
-		t.Run(testCase.label, func(t *testing.T) {
-			got := make(map[string]interface{})
-			err := DeSerialize(testCase.input, &got)
-			if err != nil {
-				if testCase.errMsg == "" {
-					t.Fatalf("DeSerialize method return an un-expected (%s)", err)
-				}
-				if !strings.Contains(string(err.Error()), testCase.errMsg) {
-					t.Fatalf("DeSerialize method returned an error (%s)", err)
-				}
-			} else {
-				if !reflect.DeepEqual(testCase.expected, got) {
-					t.Errorf("Serialize returned unexpected : %v;"+
-						" expected %v", got, testCase.expected)
-				}
-			}
-		})
-	}
 }

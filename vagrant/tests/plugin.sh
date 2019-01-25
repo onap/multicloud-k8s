4593ee6de1a90edc20f7a56ee5e4edec91d966cc
Kiran Kamineni <kiran.k.kamineni@intel.com>
2019-01-25 12:32:55 -0800

    Add test helm chart for testing
    Add a test helm chart for testing the end to end
    flow. This chart brings up a service and a deployment.
    The deployment brings up two containers, vault and consul.
    Vault and Consul are brought up in dev mode.
    The helm chart also has a subchart, common, which contains
    templates that are used within the main chart.
    Added an override deployment.yaml which creates only one
    container instead of two
    Fixed plugin.sh and tar file creation steps
    
    Issue-ID: MULTICLOUD-291
    Change-Id: I7eb10c9f43f8a4e09938139b5e2edee5d59feb3b
    Signed-off-by: Kiran Kamineni <kiran.k.kamineni@intel.com>
    Signed-off-by: Victor Morales <victor.morales@intel.com>

--- vagrant/tests/plugin.sh
+++ vagrant/tests/plugin.sh
@@ -16,10 +16,13 @@ set -o pipefail
 source _common.sh
 source _functions.sh
 
-base_url="http://localhost:8081/v1/vnf_instances/"
+base_url="http://localhost:8081"
 cloud_region_id="krd"
 namespace="default"
 csar_id="94e414f6-9ca4-11e8-bb6a-52540067263b"
+rbd_csar_id="7eb09e38-4363-9942-1234-3beb2e95fd85"
+definition_id="9d117af8-30b8-11e9-af94-525400277b3d"
+profile_id="ebe353d2-30b7-11e9-9515-525400277b3d"
 
 # _build_generic_sim() - Creates a generic simulator image in case that doesn't exist
 function _build_generic_sim {
@@ -56,42 +59,84 @@ destroy_deployment $plugin_deployment_name
 #start_aai_service
 populate_CSAR_plugin $csar_id
 
+# Teardown
+teardown $plugin_deployment_name
+
+# Setup
+populate_CSAR_rbdefinition $rbd_csar_id
+
 # Test
+print_msg "Create Resource Bundle Definition Metadata"
 payload_raw="
 {
-    \"cloud_region_id\": \"$cloud_region_id\",
-    \"namespace\": \"$namespace\",
-    \"csar_id\": \"$csar_id\"
+    \"name\": \"test-rbdef\",
+    \"chart-name\": \"vault-consul-dev\",
+    \"description\": \"testing resource bundle definition api\",
+    \"uuid\": \"$definition_id\",
+    \"service-type\": \"firewall\"
 }
 "
 payload=$(echo $payload_raw | tr '\n' ' ')
-echo "Creating VNF Instance"
-vnf_id=$(curl -s -d "$payload" "${base_url}" | jq -r '.vnf_id')
-echo "=== Validating Kubernetes ==="
-kubectl get --no-headers=true --namespace=${namespace} deployment ${cloud_region_id}-${namespace}-${vnf_id}-${plugin_deployment_name}
-kubectl get --no-headers=true --namespace=${namespace} service ${cloud_region_id}-${namespace}-${vnf_id}-${plugin_service_name}
-echo "VNF Instance created succesfully with id: $vnf_id"
+rbd_id=$(curl -s -d "$payload" -X POST "${base_url}/v1/rb/definition" | jq -r '.uuid')
 
-vnf_id_list=$(curl -s -X GET "${base_url}${cloud_region_id}/${namespace}" | jq -r '.vnf_id_list')
-if [[ "$vnf_id_list" != *"${vnf_id}"* ]]; then
-    echo $vnf_id_list
-    echo "VNF Instance not stored"
+print_msg "Upload Resource Bundle Definition Content"
+curl -s --data-binary @${CSAR_DIR}/${rbd_csar_id}/${rbd_content_tarball}.gz -X POST "${base_url}/v1/rb/definition/$rbd_id/content"
+
+print_msg "Listing Resource Bundle Definitions"
+rbd_id_list=$(curl -s -X GET "${base_url}/v1/rb/definition")
+if [[ "$rbd_id_list" != *"${rbd_id}"* ]]; then
+    echo $rbd_id_list
+    echo "Resource Bundle Definition not stored"
     exit 1
 fi
 
-vnf_details=$(curl -s -X GET "${base_url}${cloud_region_id}/${namespace}/${vnf_id}")
-if [[ -z "$vnf_details" ]]; then
-    echo "Cannot retrieved VNF Instance details"
+print_msg "Create Resource Bundle Profile Metadata"
+kubeversion=$(kubectl version | grep 'Server Version' | awk -F '"' '{print $6}')
+payload_raw="
+{
+    \"name\": \"test-rbprofile\",
+    \"namespace\": \"$namespace\",
+    \"rbdid\": \"$definition_id\",
+    \"uuid\": \"$profile_id\",
+    \"kubernetesversion\": \"$kubeversion\"
+}
+"
+payload=$(echo $payload_raw | tr '\n' ' ')
+rbp_id=$(curl -s -d "$payload" -X POST "${base_url}/v1/rb/profile" | jq -r '.uuid')
+
+print_msg "Upload Resource Bundle Profile Content"
+curl -s --data-binary @${CSAR_DIR}/${rbd_csar_id}/${rbp_content_tarball}.gz -X POST "${base_url}/v1/rb/profile/$rbp_id/content"
+
+print_msg "Listing Resource Bundle Profiles"
+rbp_id_list=$(curl -s -X GET "${base_url}/v1/rb/profile")
+if [[ "$rbp_id_list" != *"${rbp_id}"* ]]; then
+    echo $rbd_id_list
+    echo "Resource Bundle Profile not stored"
     exit 1
 fi
-echo "VNF details $vnf_details"
 
-echo "Deleting $vnf_id VNF Instance"
-curl -X DELETE "${base_url}${cloud_region_id}/${namespace}/${vnf_id}"
-if [[ 200 -eq $(curl -o /dev/null -w %{http_code} -s -X GET "${base_url}${cloud_region_id}/${namespace}/${vnf_id}") ]]; then
+print_msg "Instantiate Profile"
+payload_raw="
+{
+    \"cloud_region_id\": \"$cloud_region_id\",
+    \"rb_profile_id\": \"$profile_id\",
+    \"csar_id\": \"$rbd_csar_id\"
+}
+"
+payload=$(echo $payload_raw | tr '\n' ' ')
+vnf_id=$(curl -s -d "$payload" "${base_url}/v1/vnf_instances/" | jq -r '.vnf_id')
+
+print_msg "Deleting $rbd_id Resource Bundle Definition"
+curl -X DELETE "${base_url}/v1/rb/definition/$rbd_id"
+if [[ 500 -ne $(curl -o /dev/null -w %{http_code} -s -X GET "${base_url}/v1/rb/definition/$rbd_id") ]]; then
+    echo "Resource Bundle Definition not deleted"
+# TODO: Change the HTTP code for 404 when the resource is not found in the API
+    exit 1
+fi
+
+print_msg "Deleting $vnf_id VNF Instance"
+curl -X DELETE "${base_url}/v1/vnf_instances/${cloud_region_id}/${namespace}/${vnf_id}"
+if [[ 404 -ne $(curl -o /dev/null -w %{http_code} -s -X GET "${base_url}${cloud_region_id}/${namespace}/${vnf_id}") ]]; then
     echo "VNF Instance not deleted"
     exit 1
 fi
-
-# Teardown
-teardown $plugin_deployment_name

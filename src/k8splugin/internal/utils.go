e8dd2634732fa868a31a3c49e0af99a71d51d40a
Victor Morales <victor.morales@intel.com>
2019-01-30 14:44:38 -0800

    Create functions to get ESR info
    The ESR portal is used to register VIM information. As part of the
    end-to-end integration the kubeconfig file is expected to be retrieved
    from this endpoint. This change implements the functions to comunicate
    with AAI endpoint.
    
    Change-Id: I20a932cb0417c3ac4f24c0b4760ebfbf47682c9b
    Signed-off-by: Victor Morales <victor.morales@intel.com>
    Issue-ID: MULTICLOUD-470

--- src/k8splugin/internal/utils.go
+++ src/k8splugin/internal/utils.go
@@ -14,9 +14,12 @@ limitations under the License.
 package utils
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"k8splugin/internal/db"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"plugin"
@@ -68,7 +71,8 @@ var DecodeYAML = func(path string, into runtime.Object) (runtime.Object, error) 
 // CheckEnvVariables checks for required Environment variables
 func CheckEnvVariables() error {
 	envList := []string{"CSAR_DIR", "KUBE_CONFIG_DIR", "PLUGINS_DIR",
-		"DATABASE_TYPE", "DATABASE_IP", "OVN_CENTRAL_ADDRESS"}
+		"DATABASE_TYPE", "DATABASE_IP", "OVN_CENTRAL_ADDRESS",
+		"AAI_SERVICE_URL", "AAI_USERNAME", "AAI_PASSWORD"}
 	for _, env := range envList {
 		if _, ok := os.LookupEnv(env); !ok {
 			return pkgerrors.New("environment variable " + env + " not set")
@@ -132,4 +136,117 @@ func CheckInitialSettings() error {
 	}
 
 	return nil
+}
+
+// Vim contains information about the Cloud Region registered in ESR
+type Vim struct {
+	CloudOwner         string `json:"cloud-owner"`
+	CloudRegionID      string `json:"cloud-region-id"`
+	CloudType          string `json:"cloud-type"`
+	OwnerDefinedType   string `json:"owner-defined-type"`
+	CloudRegionVersion string `json:"cloud-region-version"`
+	CloudZone          string `json:"cloud-zone"`
+}
+
+// VimAuth contains the authentication data to connect to a remote Cloud registered in ESR
+type VimAuth struct {
+	ESRSystemInfoID string `json:"esr-system-info-id"`
+	ServiceURL      string `json:"service-url"`
+	Username        string `json:"user-name"`
+	Password        string `json:"password"`
+	SystemType      string `json:"system-type"`
+	SslInsecure     bool   `json:"ssl-insecure"`
+	CloudDomain     string `json:"cloud-domain"`
+	ResourceVersion string `json:"resource-version"`
+}
+
+// GetESRInfo retrieves the Cloud information stored from the ESR GUI portal
+func GetESRInfo(cloudOwner, cloudRegionID string) (*Vim, error) {
+	if len(cloudOwner) == 0 {
+		return nil, pkgerrors.New("cloudOwner empty value")
+	}
+	if len(cloudRegionID) == 0 {
+		return nil, pkgerrors.New("cloudRegionID empty value")
+	}
+	body, err := doAAIRequest("GET",
+		"/cloud-infrastructure/cloud-regions/cloud-region/"+cloudOwner+"/"+cloudRegionID, "")
+	if err != nil {
+		return nil, pkgerrors.Wrap(err, "The request to retrieve the ESR information failed")
+	}
+
+	var vim *Vim
+	err = json.Unmarshal([]byte(body), &vim)
+	if err != nil {
+		return nil, pkgerrors.Wrap(err, "The AAI body response is a invalid VIM information")
+	}
+
+	return vim, nil
+}
+
+// GetESRAuthInfo retrieves the Cloud authentication information stored from the ESR GUI portal
+// to connect to a remote Cloud
+func GetESRAuthInfo(cloudOwner, cloudRegionID string) (*VimAuth, error) {
+	if len(cloudOwner) == 0 {
+		return nil, pkgerrors.New("cloudOwner empty value")
+	}
+	if len(cloudRegionID) == 0 {
+		return nil, pkgerrors.New("cloudRegionID empty value")
+	}
+	body, err := doAAIRequest("GET",
+		"/cloud-infrastructure/cloud-regions/cloud-region/"+cloudOwner+"/"+cloudRegionID+
+			"/esr-system-info-list/esr-system-info/", "")
+	if err != nil {
+		return nil, pkgerrors.Wrap(err, "The request to retrieve the ESR Authentication information failed")
+	}
+
+	var auth *VimAuth
+	err = json.Unmarshal([]byte(body), &auth)
+	if err != nil {
+		return nil, pkgerrors.Wrap(err, "The AAI body response is a invalid VIM authentication data")
+	}
+
+	return auth, nil
+}
+
+func doAAIRequest(httpMethod, path, payload string) ([]byte, error) {
+	aaiURL, err := url.Parse(os.Getenv("AAI_SERVICE_URL"))
+	if err != nil && aaiURL.Scheme != "http" && aaiURL.Scheme != "https" {
+		return nil, pkgerrors.New("The AAI_SERVICE_URL value is invalid")
+	}
+	aaiUser := os.Getenv("AAI_USERNAME")
+	aaiPassword := os.Getenv("AAI_PASSWORD")
+	aaiSchemaVersion := os.Getenv("AAI_SCHEMA_VERSION")
+	if len(aaiSchemaVersion) == 0 {
+		aaiSchemaVersion = "v14"
+	}
+	aaiURL.Path = "/aai/" + aaiSchemaVersion + "/" + path
+
+	request, err := http.NewRequest(httpMethod, aaiURL.String(), strings.NewReader(payload))
+	if err != nil {
+		return nil, pkgerrors.Wrap(err, "The AAI request can't be created")
+	}
+	request.SetBasicAuth(aaiUser, aaiPassword)
+	headers := map[string]string{
+		"Accept":          "application/json",
+		"Content-Type":    "application/json",
+		"X-TransactionId": "testaai",
+		"X-FromAppId":     "AAI",
+	}
+	for k, v := range headers {
+		request.Header.Add(k, v)
+	}
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, pkgerrors.Wrap(err, "There is a problem during the execution of the AAI request")
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, pkgerrors.Wrap(err, "The AAI body response can't be read")
+	}
+
+	return body, nil
 }

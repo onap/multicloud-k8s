dfdd9bd312591ffe57df3f9ea58031bf5ea96c11
Victor Morales <victor.morales@intel.com>
2019-01-30 14:44:38 -0800

    Create functions to get ESR info
    The ESR portal is used to register VIM information. As part of the
    end-to-end integration the kubeconfig file is expected to be retrieved
    from this endpoint. This change implements the functions to comunicate
    with AAI endpoint.
    
    Change-Id: I20a932cb0417c3ac4f24c0b4760ebfbf47682c9b
    Signed-off-by: Victor Morales <victor.morales@intel.com>
    Issue-ID: MULTICLOUD-470

--- src/k8splugin/internal/utils_test.go
+++ src/k8splugin/internal/utils_test.go
@@ -16,6 +16,9 @@ limitations under the License.
 package utils
 
 import (
+	"net/http"
+	"os"
+	"reflect"
 	"strings"
 	"testing"
 
@@ -23,6 +26,8 @@ import (
 	coreV1 "k8s.io/api/core/v1"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+
+	"net/http/httptest"
 )
 
 func TestDecodeYAML(t *testing.T) {
@@ -89,6 +94,214 @@ func TestDecodeYAML(t *testing.T) {
 
 				// 	t.Fatalf("Decode YAML method returned: \n%v\n and it was expected: \n%v", result, testCase.expectedResult)
 				// }
+			}
+		})
+	}
+}
+
+func TestGetESRInfo(t *testing.T) {
+	testCases := []struct {
+		label          string
+		input          map[string]string
+		mockResponse   map[string]interface{}
+		expectedError  string
+		expectedResult *Vim
+	}{
+		{
+			label:         "Empty cloudOwner value failure",
+			expectedError: "cloudOwner empty value",
+		},
+		{
+			label: "Empty cloudRegionID value failure",
+			input: map[string]string{
+				"cloudOwner": "VIM-test1",
+			},
+			expectedError: "cloudRegionID empty value",
+		},
+		{
+			label: "Fail to parse the body AAI information",
+			mockResponse: map[string]interface{}{
+				"code": 500,
+			},
+			input: map[string]string{
+				"cloudOwner":    "VIM-test1",
+				"cloudRegionID": "RegionOne1",
+			},
+			expectedError: "The AAI body response is a invalid VIM information",
+		},
+		{
+			label: "Sucess to retrieve ESR VIM information",
+			input: map[string]string{
+				"cloudOwner":    "VIM-test1",
+				"cloudRegionID": "RegionOne1",
+			},
+			mockResponse: map[string]interface{}{
+				"body": []byte(`{
+					"cloud-owner":"VIM-test1",
+					"cloud-region-id":"RegionOne1",
+					"cloud-type":"kubernetes",
+					"owner-defined-type":"owner-defined-type",
+					"cloud-region-version":"v1.0",
+					"cloud-zone":"cloud zone",
+					"complex-name":"complex name",
+					"cloud-extra-info":"{}",
+					"resource-version":"1548686509260"
+				}`),
+			},
+			expectedResult: &Vim{
+				CloudOwner:         "VIM-test1",
+				CloudRegionID:      "RegionOne1",
+				CloudType:          "kubernetes",
+				OwnerDefinedType:   "owner-defined-type",
+				CloudRegionVersion: "v1.0",
+				CloudZone:          "cloud zone",
+				ComplexName:        "complex name",
+				CloudExtraInfo:     "{}",
+				ResourceVersion:    "1548686509260",
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.label, func(t *testing.T) {
+			testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+				if val, ok := testCase.mockResponse["code"]; ok {
+					if code, ok := val.(int); ok {
+						res.WriteHeader(code)
+					}
+				}
+				if val, ok := testCase.mockResponse["body"]; ok {
+					if body, ok := val.([]byte); ok {
+						res.Write(body)
+					}
+				}
+			}))
+			defer func() { testServer.Close() }()
+			os.Setenv("AAI_SERVICE_URL", testServer.URL)
+
+			result, err := GetESRInfo(testCase.input["cloudOwner"], testCase.input["cloudRegionID"])
+			if err != nil {
+				if testCase.expectedError == "" {
+					t.Fatalf("ESR information get method return an un-expected (%s)", err)
+				}
+				if !strings.Contains(string(err.Error()), testCase.expectedError) {
+					t.Fatalf("ESR information get method returned an error (%s)", err)
+				}
+			} else {
+				if testCase.expectedError != "" && testCase.expectedResult == nil {
+					t.Fatalf("ESR information get method was expecting \"%s\" error message", testCase.expectedError)
+				}
+				if result == nil {
+					t.Fatal("ESR information get method returned nil result")
+				}
+				if !reflect.DeepEqual(testCase.expectedResult, result) {
+
+					t.Fatalf("ESR information get method returned: \n%v\n and it was expected: \n%v", result, testCase.expectedResult)
+				}
+			}
+		})
+	}
+}
+
+func TestGetESRAuthInfo(t *testing.T) {
+	testCases := []struct {
+		label          string
+		input          map[string]string
+		mockResponse   map[string]interface{}
+		expectedError  string
+		expectedResult *VimAuth
+	}{
+		{
+			label:         "Empty cloudOwner value failure",
+			expectedError: "cloudOwner empty value",
+		},
+		{
+			label: "Empty cloudRegionID value failure",
+			input: map[string]string{
+				"cloudOwner": "VIM-test1",
+			},
+			expectedError: "cloudRegionID empty value",
+		},
+		{
+			label: "Fail to parse the body AAI information",
+			mockResponse: map[string]interface{}{
+				"code": 500,
+			},
+			input: map[string]string{
+				"cloudOwner":    "VIM-test1",
+				"cloudRegionID": "RegionOne1",
+			},
+			expectedError: "The AAI body response is a invalid VIM authentication data",
+		},
+		{
+			label: "Sucess to retrieve ESR Authentication information",
+			input: map[string]string{
+				"cloudOwner":    "VIM-test1",
+				"cloudRegionID": "RegionOne1",
+			},
+			mockResponse: map[string]interface{}{
+				"body": []byte(`{
+					"esr-system-info-id":"vim-esr-system-info-id",
+					"service-url":"http://10.12.25.2:5000/v3",
+					"user-name":"demo",
+					"password":"onapdemo",
+					"system-type":"VIM",
+					"ssl-insecure":true,
+					"cloud-domain":"Default",
+					"resource-version":"1548686509260"
+					}`),
+			},
+			expectedResult: &VimAuth{
+				ESRSystemInfoID: "vim-esr-system-info-id",
+				ServiceURL:      "http://10.12.25.2:5000/v3",
+				Username:        "demo",
+				Password:        "onapdemo",
+				SystemType:      "VIM",
+				SslInsecure:     true,
+				CloudDomain:     "Default",
+				ResourceVersion: "1548686509260",
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.label, func(t *testing.T) {
+			testServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+				if val, ok := testCase.mockResponse["code"]; ok {
+					if code, ok := val.(int); ok {
+						res.WriteHeader(code)
+					}
+				}
+				if val, ok := testCase.mockResponse["body"]; ok {
+					if body, ok := val.([]byte); ok {
+						res.Write(body)
+					}
+				}
+			}))
+			defer func() { testServer.Close() }()
+			os.Setenv("AAI_SERVICE_URL", testServer.URL)
+			os.Setenv("AAI_USERNAME", "AAI")
+			os.Setenv("AAI_PASSWORD", "AAI")
+
+			result, err := GetESRAuthInfo(testCase.input["cloudOwner"], testCase.input["cloudRegionID"])
+			if err != nil {
+				if testCase.expectedError == "" {
+					t.Fatalf("The get method that retrieves ESR Authentication data return an un-expected (%s)", err)
+				}
+				if !strings.Contains(string(err.Error()), testCase.expectedError) {
+					t.Fatalf("The get method that retrieves ESR Authentication data returned an error (%s)", err)
+				}
+			} else {
+				if testCase.expectedError != "" && testCase.expectedResult == nil {
+					t.Fatalf("The get method that retrieves ESR Authentication data was expecting \"%s\" error message", testCase.expectedError)
+				}
+				if result == nil {
+					t.Fatal("The get method that retrieves ESR Authentication data returned nil result")
+				}
+				if !reflect.DeepEqual(testCase.expectedResult, result) {
+
+					t.Fatalf("The get method that retrieves ESR Authentication data method returned: \n%v\n and it was expected: \n%v", result, testCase.expectedResult)
+				}
 			}
 		})
 	}

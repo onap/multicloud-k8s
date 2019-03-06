0e112582a941a6ecc9ae43aa5f802fd467e4f7c1
Akhila Kishore <akhila.kishore@intel.com>
2019-03-06 06:34:09 -0800

    Work in Progress. Restructuring the repo.
    Issue-ID: MULTICLOUD-301
    Signed-off-by: Akhila Kishore <akhila.kishore@intel.com>
    The idea is to restructure the existing repo create a deployment
    independent of Vagrant or other hosting providers. Made updates to
    sync and ssh keys. Updated deployments path. P6:removed log files and
    admin.conf since they are auto generated.
    P7: Renamed KRD to KUbernetes Deploy(Kud) including the ansible scripts
    Added new path to functional tests.
    Moved samples pdfs to sites. Minor changes to Readme.
    Removed krd files. p9:Updated aio.sh, moved sample config
    Corrected other nits.
    
    Change-Id: Ie48c26b12ab58b604493fba58a9c5b9f8ba10942
    Signed-off-by: Akhila Kishore <akhila.kishore@intel.com>

---
+++ kud/tests/tests/plugin_edgex.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o errexit
+set -o nounset
+set -o pipefail
+#set -o xtrace
+
+source _functions.sh
+
+base_url="http://localhost:8081/v1/vnf_instances/"
+cloud_region_id="kud"
+namespace="default"
+csar_id=cb009bfe-bbee-11e8-9766-525400435678
+
+# Setup
+_checks_args ${csar_id}
+cp -R ./edgex/* ${CSAR_DIR}/${csar_id}/
+
+# Test
+payload_raw="
+{
+    \"cloud_region_id\": \"$cloud_region_id\",
+    \"namespace\": \"$namespace\",
+    \"csar_id\": \"$csar_id\"
+}
+"
+payload=$(echo $payload_raw | tr '\n' ' ')
+
+echo "Creating EdgeX VNF Instance"
+
+vnf_id=$(curl -s -d "$payload" "${base_url}" | jq -r '.vnf_id')
+
+echo "=== Validating Kubernetes ==="
+kubectl get --no-headers=true --namespace=${namespace} deployment ${cloud_region_id}-${namespace}-${vnf_id}-edgex-core-command
+kubectl get --no-headers=true --namespace=${namespace} service ${cloud_region_id}-${namespace}-${vnf_id}-edgex-core-command
+echo "VNF Instance created succesfully with id: $vnf_id"
+
+# TODO: Add heath checks to verify EdgeX services
+
+vnf_id_list=$(curl -s -X GET "${base_url}${cloud_region_id}/${namespace}" | jq -r '.vnf_id_list')
+if [[ "$vnf_id_list" != *"${vnf_id}"* ]]; then
+    echo $vnf_id_list
+    echo "VNF Instance not stored"
+    exit 1
+fi
+
+vnf_details=$(curl -s -X GET "${base_url}${cloud_region_id}/${namespace}/${vnf_id}")
+if [[ -z "$vnf_details" ]]; then
+    echo "Cannot retrieved VNF Instance details"
+    exit 1
+fi
+echo "VNF details $vnf_details"
+
+echo "Deleting $vnf_id VNF Instance"
+curl -X DELETE "${base_url}${cloud_region_id}/${namespace}/${vnf_id}"
+if [[ -n $(curl -s -X GET "${base_url}${cloud_region_id}/${namespace}/${vnf_id}") ]]; then
+    echo "VNF Instance not deleted"
+    exit 1
+fi
+
+# Teardown

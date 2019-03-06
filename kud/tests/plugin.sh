b1a9a85e0074546d3672abaa2ea26cb5553da901
Akhila Kishore <akhila.kishore@intel.com>
2019-03-06 06:34:09 -0800

    Restructuring the repo.
    The idea is to restructure the existing repo create a deployment
    independent of Vagrant or other hosting providers. Made updates to
    sync and ssh keys. Updated deployments path. P6:removed log files and
    admin.conf since they are auto generated.
    P7: Renamed KRD to KUbernetes Deploy(Kud) including the ansible scripts
    Added new path to functional tests.
    Moved samples pdfs to sites. Minor changes to Readme.
    Removed krd files. p9:Updated aio.sh, moved sample config
    Corrected other nits. p:10 updated and verified test cases.
    Addressed comments and changes associated with it.
    Updated Readme and minor change in Vagrantfile.
    Validated test cases again.
    
    Issue-ID: MULTICLOUD-301
    Change-Id: Ie48c26b12ab58b604493fba58a9c5b9f8ba10942
    Signed-off-by: Akhila Kishore <akhila.kishore@intel.com>

--- kud/tests/plugin.sh
+++ kud/tests/plugin.sh
@@ -17,7 +17,7 @@ source _common.sh
 source _functions.sh
 
 base_url="http://localhost:8081"
-cloud_region_id="krd"
+cloud_region_id="kud"
 namespace="default"
 csar_id="94e414f6-9ca4-11e8-bb6a-52540067263b"
 rbd_csar_id="7eb09e38-4363-9942-1234-3beb2e95fd85"

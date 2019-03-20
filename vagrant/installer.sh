58f8933d2df7c2601bbdff7bfec1652b48946138
Ritu Sood <ritu.sood@intel.com>
2019-03-20 09:17:10 -0700

    Workaround for Kernel Package Issue
    Due to Ubuntu Kernel package issue
    KRD is not installing.
    https://bugs.launchpad.net/ubuntu/+source/linux-base/+bug/1820755
    
    This is a workaround to not install
    those kernel packages
    
    Change-Id: Ib298ad695c2e7b88bef46340afc9407868820e1d
    Signed-off-by: Ritu Sood <ritu.sood@intel.com>
    Issue-ID: MULTICLOUD-301

--- vagrant/installer.sh
+++ vagrant/installer.sh
@@ -55,7 +55,7 @@ function _install_docker {
     if $(docker version &>/dev/null); then
         return
     fi
-    sudo apt-get install -y software-properties-common linux-image-extra-$(uname -r) linux-image-extra-virtual apt-transport-https ca-certificates curl
+    sudo apt-get install -y apt-transport-https ca-certificates curl
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt-get update

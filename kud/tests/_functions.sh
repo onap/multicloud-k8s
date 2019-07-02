5c8796c977107e26db88b2ad51d8fb99416aa4eb
Konrad Bańka <k.banka@samsung.com>
2019-07-02 09:07:49 +0200

    Fix provisioning central IP address for ovn tests
    After installing ovn-kubernetes addon, previous physical
    interface becomes enslaved under ovs bridge, that intercepts
    his original IP address. Relevant function has been fixed to enable
    reusing IP address determined at provision time, or if not available
    or forced, determine correct interface and query for its address.
    
    Issue-ID: MULTICLOUD-686
    
    Signed-off-by: Konrad Bańka <k.banka@samsung.com>
    Change-Id: I156297098b7919471df711f049f0d9439bd81420

--- kud/tests/_functions.sh
+++ kud/tests/_functions.sh
@@ -25,10 +25,17 @@ function print_msg {
 }
 
 function get_ovn_central_address {
+    #Reuse OVN_CENTRAL_ADDRESS if available (bypassable by --force flag)
+    if [[ "${1:-}" != "--force" ]] && [[ -n "${OVN_CENTRAL_ADDRESS:-}" ]]; then
+        echo "${OVN_CENTRAL_ADDRESS}"
+        return 0
+    fi
+
+    #Determine OVN_CENTRAL_INTERFACE address
     ansible_ifconfig=$(ansible ovn-central[0] -i ${FUNCTIONS_DIR}/../hosting_providers/vagrant/inventory/hosts.ini -m shell -a "ifconfig ${OVN_CENTRAL_INTERFACE} |grep \"inet addr\" |awk '{print \$2}' |awk -F: '{print \$2}'")
     if [[ $ansible_ifconfig != *CHANGED* ]]; then
-        echo "Fail to get the OVN central IP address from ${OVN_CENTRAL_INTERFACE} nic"
-        exit
+        echo "Fail to get the OVN central IP address from ${OVN_CENTRAL_INTERFACE} nic" >&2
+        return 1
     fi
     echo "$(echo ${ansible_ifconfig#*>>} | tr '\n' ':')6641"
 }

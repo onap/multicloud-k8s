9ade6bfbedbbc4890386b5d44dcad1bd6e80a9d6
sanjaymekhale <sm00557598@techmahindra.com>
2019-10-16 17:07:27 +0530

    Correcting build failure
    Moved setFormatter to other location.
    
    Issue-ID: MULTICLOUD-577
    Change-Id: I05e2acbfbb5dd79bd26df73e0ad64c2068b9f6e5
    Signed-off-by: sanjaymekhale <sm00557598@techmahindra.com>

---
+++ src/k8splugin/internal/logs/logger.go
@@ -0,0 +1,14 @@
+package logs
+
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+}
+
+func WithFields(msg string, fkey string, fvalue string) {
+	log.WithFields(log.Fields{fkey: fvalue}).Error(msg)
+}

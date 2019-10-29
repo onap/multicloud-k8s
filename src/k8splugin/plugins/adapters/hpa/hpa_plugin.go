c70e753bcedc3c7551922f08ff81c57ad27ffa2f
Marcus G K Williams <marcus.williams@intel.com>
2019-10-29 15:22:10 -0700

    Draft Interface for HPA Placement Plugin
    Issue-ID: MULTICLOUD-729
    Signed-off-by: Marcus G K Williams <marcus.williams@intel.com>
    Change-Id: Iaee642a43e4dae892005418f458cc65c129424fd

---
+++ src/k8splugin/plugins/adapters/hpa/hpa_plugin.go
@@ -0,0 +1,77 @@
+/*
+Copyright 2019 Intel Corporation.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"errors"
+
+	"github.com/onap/multicloud-k8s/src/k8splugin/internal/plugin"
+)
+
+// Compile check - does hpaPlacementPlugin implements the correct interface?
+var _ plugin.PlacementAdapter = hpaPlacementPlugin{}
+
+// ExportedVariable is what we will look for when calling the plugin
+var ExportedVariable hpaPlacementPlugin
+
+type hpaPlacementPlugin struct {
+}
+
+// Intent
+
+// Validate and Store Intent
+func (p hpaPlacementPlugin) StoreIntent(intent interface{}) error {
+	return errors.New("Error")
+}
+
+// Validate and Modify Intent
+func (p hpaPlacementPlugin) ModifyIntent(intentName string, intent interface{}) error {
+	return errors.New("Error")
+}
+
+// Delete Intent
+func (p hpaPlacementPlugin) DeleteIntent(intentName string) error {
+	return errors.New("Error")
+}
+
+// Get Intent
+func (p hpaPlacementPlugin) GetIntent(intentName string) (map[string]string, error) {
+	//
+	return make(map[string]string), errors.New("Error")
+}
+
+// Site
+
+// Get Valid Sites to deploy workload given intents and sites
+func (p hpaPlacementPlugin) GetSites(intentName string, resourceBundleName string, sites []string) (map[string]string, error) {
+	return make(map[string]string), errors.New("Error")
+}
+
+// Node Features
+
+func (p hpaPlacementPlugin) StoreFeatures(Features interface{}) error {
+	return errors.New("Error")
+}
+func (p hpaPlacementPlugin) GetFeaturesPerNode(clusterName string) (map[string]string, error) {
+	return make(map[string]string), errors.New("Error")
+}
+func (p hpaPlacementPlugin) GetFeatures(clusterName string) (map[string]string, error) {
+	return make(map[string]string), errors.New("Error")
+}
+func (p hpaPlacementPlugin) DeleteFeatures(clusterName string) error {
+	return errors.New("Error")
+}
+func (p hpaPlacementPlugin) ModifyFeatures(clusterName string, Features interface{}) error {
+	return errors.New("Error")
+}

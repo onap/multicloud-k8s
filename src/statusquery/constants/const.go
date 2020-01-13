0028fe82bddd4562f68166c913ffab6714006edc
vamshi.nemalikonda <vn00480215@techmahindra.com>
2020-01-13 18:56:04 +0530

    Updating AAI upon successful instantiation of resource bundle
    Listens to Status API exposed by k8s, updates accordingly to AAI.Issue-ID: MULTICLOUD-457
    
    Change-Id: Icf97d13a6c683b8f09491491ed2d2709f3500104
    Signed-off-by: vamshi.nemalikonda <vn00480215@techmahindra.com>

---
+++ src/statusquery/constants/const.go
@@ -0,0 +1,23 @@
+package constants
+
+const (
+        XFromAppId     = "SO"
+        ContentType    = "application/json"
+        Accept         = "application/json"
+        XTransactionId = "get_aai_subscr"
+        Authorization  = "Basic QUFJOkFBSQ=="
+)
+
+const (
+        AAI_EP = "/aai/v14"
+        AAI_CEP =  "/cloud-infrastructure/cloud-regions/"
+        AAI_NEP = "/network/generic-vnfs/generic-vnf/"
+        AAI_URI      = "https://aai-host"
+        AAI_Port     = "30233"
+)
+
+const (
+        MK8S_EP = "/api/multicloud-k8s/v1/v1/instance/"
+        MK8S_URI      = "https://multicloud-host"
+        MK8S_Port     = "30280"
+)

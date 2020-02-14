a604eb26964a6cb8c595452c0d7b66eeedcc95c9
enyinna1234 <enyinna.ochulor@intel.com>
2020-02-14 08:41:49 -0800

    Add Distributed Cloud Manager
    This handles RESTful API CRUD operations for logical clouds, CLuster,
    User Permissions, Quota, and Key Value pairs.
    See: https://wiki.onap.org/x/tAiVB
    
    Issue-ID: MULTICLOUD-996
    Signed-off-by: Enyinna Ochulor <enyinna.ochulor@intel.com>
    Change-Id: I654a304cd682f762c02cfd92b4483d1edea63fca

---
+++ src/dcm/api/clusterHandler.go
@@ -0,0 +1,162 @@
+/*
+* Copyright 2020 Intel Corporation, Inc
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+* implied.
+* See the License for the specific language governing permissions
+* and
+* limitations under the License.
+*/
+
+package api
+
+import (
+    "encoding/json"
+    "net/http"
+    "io"
+    "github.com/onap/multicloud-k8s/src/dcm/pkg/module"
+
+    "github.com/gorilla/mux"
+)
+
+
+// clusterHandler is used to store backend implementations objects
+type clusterHandler struct {
+    client module.ClusterManager
+}
+
+// CreateHandler handles creation of the cluster reference entry in the database
+
+func (h clusterHandler) createHandler(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    project := vars["project-name"]
+    logicalCloud := vars["logical-cloud-name"]
+    var v module.Cluster
+
+    err := json.NewDecoder(r.Body).Decode(&v)
+    switch {
+    case err == io.EOF:
+        http.Error(w, "Empty body", http.StatusBadRequest)
+        return
+    case err != nil:
+        http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+        return
+    }
+
+    // Cluster Reference Name is required.
+    if v.MetaData.ClusterReference == "" {
+        http.Error(w, "Missing name in POST request", http.StatusBadRequest)
+        return
+    }
+
+    ret, err := h.client.CreateCluster(project, logicalCloud, v)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    err = json.NewEncoder(w).Encode(ret)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+}
+
+// getHandler handle GET operations on a particular name
+// Returns a Cluster Reference
+func (h clusterHandler) getHandler(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    project := vars["project-name"]
+    logicalCloud := vars["logical-cloud-name"]
+    name := vars["cluster-reference"]
+    var ret interface{}
+    var err error
+
+    if len(name) == 0 {
+        ret, err = h.client.GetAllClusters(project, logicalCloud)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+    } else {
+        ret, err = h.client.GetCluster(project, logicalCloud, name)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    err = json.NewEncoder(w).Encode(ret)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+}
+
+// UpdateHandler handles Update operations on a particular cluster reference
+func (h clusterHandler) updateHandler(w http.ResponseWriter, r *http.Request) {
+    var v module.Cluster
+    vars := mux.Vars(r)
+    project := vars["project-name"]
+    logicalCloud := vars["logical-cloud-name"]
+    name := vars["cluster-reference"]
+
+    err := json.NewDecoder(r.Body).Decode(&v)
+    switch {
+        case err == io.EOF:
+            http.Error(w, "Empty body", http.StatusBadRequest)
+            return
+        case err != nil:
+            http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+            return
+    }
+
+// Name is required.
+    if v.MetaData.ClusterReference == "" {
+        http.Error(w, "Missing name in PUT request", http.StatusBadRequest)
+        return
+    }
+
+    ret, err := h.client.UpdateCluster(project, logicalCloud, name, v)
+    if err != nil {
+        http.Error(w, err.Error(),
+        http.StatusInternalServerError)
+        return
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    err = json.NewEncoder(w).Encode(ret)
+    if err != nil {
+        http.Error(w, err.Error(),
+        http.StatusInternalServerError)
+        return
+    }
+}
+
+//deleteHandler handles DELETE operations on a particular record
+func (h clusterHandler) deleteHandler(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    project := vars["project-name"]
+    logicalCloud := vars["logical-cloud-name"]
+    name := vars["cluster-reference"]
+
+    err := h.client.DeleteCluster(project, logicalCloud, name)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.WriteHeader(http.StatusNoContent)
+}
---
+++ src/dcm/api/quotaHandler.go
@@ -0,0 +1,162 @@
+/*
+* Copyright 2020 Intel Corporation, Inc
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+* implied.
+* See the License for the specific language governing permissions
+* and
+* limitations under the License.
+*/
+
+package api
+
+import (
+    "encoding/json"
+    "net/http"
+    "io"
+    "github.com/onap/multicloud-k8s/src/dcm/pkg/module"
+
+    "github.com/gorilla/mux"
+)
+
+
+// quotaHandler is used to store backend implementations objects
+type quotaHandler struct {
+    client module.QuotaManager
+}
+
+// CreateHandler handles creation of the quota entry in the database
+
+func (h quotaHandler) createHandler(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    project := vars["project-name"]
+    logicalCloud := vars["logical-cloud-name"]
+    var v module.Quota
+
+    err := json.NewDecoder(r.Body).Decode(&v)
+    switch {
+    case err == io.EOF:
+        http.Error(w, "Empty body", http.StatusBadRequest)
+        return
+    case err != nil:
+        http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+        return
+    }
+
+    // Quota Name is required.
+    if v.MetaData.QuotaName == "" {
+        http.Error(w, "Missing name in POST request", http.StatusBadRequest)
+        return
+    }
+
+    ret, err := h.client.CreateQuota(project, logicalCloud, v)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    err = json.NewEncoder(w).Encode(ret)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+}
+
+// getHandler handle GET operations on a particular name
+// Returns a quota
+func (h quotaHandler) getHandler(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    project := vars["project-name"]
+    logicalCloud := vars["logical-cloud-name"]
+    name := vars["quota-name"]
+    var ret interface{}
+    var err error
+
+    if len(name) == 0 {
+        ret, err = h.client.GetAllQuotas(project, logicalCloud)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+    } else {
+        ret, err = h.client.GetQuota(project, logicalCloud, name)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    err = json.NewEncoder(w).Encode(ret)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+}
+
+// UpdateHandler handles Update operations on a particular quota
+func (h quotaHandler) updateHandler(w http.ResponseWriter, r *http.Request) {
+    var v module.Quota
+    vars := mux.Vars(r)
+    project := vars["project-name"]
+    logicalCloud := vars["logical-cloud-name"]
+    name := vars["quota-name"]
+
+    err := json.NewDecoder(r.Body).Decode(&v)
+    switch {
+        case err == io.EOF:
+            http.Error(w, "Empty body", http.StatusBadRequest)
+            return
+        case err != nil:
+            http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+            return
+    }
+
+    // Name is required.
+    if v.MetaData.QuotaName == "" {
+        http.Error(w, "Missing name in PUT request", http.StatusBadRequest)
+        return
+    }
+
+    ret, err := h.client.UpdateQuota(project, logicalCloud, name, v)
+    if err != nil {
+        http.Error(w, err.Error(),
+        http.StatusInternalServerError)
+        return
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    err = json.NewEncoder(w).Encode(ret)
+    if err != nil {
+        http.Error(w, err.Error(),
+        http.StatusInternalServerError)
+        return
+    }
+}
+
+//deleteHandler handles DELETE operations on a particular record
+func (h quotaHandler) deleteHandler(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    project := vars["project-name"]
+    logicalCloud := vars["logical-cloud-name"]
+    name := vars["quota-name"]
+
+    err := h.client.DeleteQuota(project, logicalCloud, name)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.WriteHeader(http.StatusNoContent)
+}

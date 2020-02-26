689ba13e49bf4344bea0e30175cced0d9063f010
Rajamohan Raj <rajamohan.raj@intel.com>
2020-02-26 02:07:34 +0000

    Add DeploymentIntentGroup APIs
    Implemented the routes for creation,
    deletion and getting DeploymentIntentGroup.
    
    Added routes for adding, getting and deleting intents.
    
    Issue-ID: MULTICLOUD-1002
    Signed-off-by: Rajamohan Raj <rajamohan.raj@intel.com>
    Change-Id: I07ba3744107bcf30aa30ed89caa21bf474c0d92a

---
+++ src/orchestrator/pkg/module/add_intents.go
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2020 Intel Corporation, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by Addlicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package module
+
+import (
+	"encoding/json"
+	"reflect"
+
+	"github.com/onap/multicloud-k8s/src/orchestrator/pkg/infra/db"
+
+	pkgerrors "github.com/pkg/errors"
+)
+
+// Intent shall have 2 fields - MetaData and Spec
+type Intent struct {
+	MetaData IntentMetaData `json:"metadata"`
+	Spec     IntentSpecData `json:"spec"`
+}
+
+// IntentMetaData has Name, Description, userdata1, userdata2
+type IntentMetaData struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	UserData1   string `json:"userData1"`
+	UserData2   string `json:"userData2"`
+}
+
+// IntentSpecData has Intent
+type IntentSpecData struct {
+	Intent IntentObj `json:"intent"`
+}
+
+// IntentObj has name of the generic placement intent
+type IntentObj struct {
+	Generic string `json:"generic"`
+}
+
+// ListOfIntents is a list of intents
+type ListOfIntents struct {
+	ListOfIntents []map[string]string `json:"intent"`
+}
+
+// IntentManager is an interface which exposes the IntentManager functionality
+type IntentManager interface {
+	AddIntent(a Intent, p string, ca string, v string, di string) (Intent, error)
+	GetIntent(i string, p string, ca string, v string, di string) (Intent, error)
+	DeleteIntent(i string, p string, ca string, v string, di string) error
+}
+
+// IntentKey consists of Name if the intent, Project name, CompositeApp name,
+// CompositeApp version
+type IntentKey struct {
+	Name                  string `json:"name"`
+	Project               string `json:"project"`
+	CompositeApp          string `json:"compositeapp"`
+	Version               string `json:"version"`
+	DeploymentIntentGroup string `json:"deployment-intent-group-name"`
+}
+
+// We will use json marshalling to convert to string to
+// preserve the underlying structure.
+func (ik IntentKey) String() string {
+	out, err := json.Marshal(ik)
+	if err != nil {
+		return ""
+	}
+	return string(out)
+}
+
+// IntentClient implements the AddIntentManager interface
+type IntentClient struct {
+	storeName   string
+	tagMetaData string
+}
+
+// NewIntentClient returns an instance of AddIntentClient
+func NewIntentClient() *IntentClient {
+	return &IntentClient{
+		storeName:   "orchestrator",
+		tagMetaData: "addintent",
+	}
+}
+
+// AddIntent adds a given intent to the deployment-intent-group and stores in the db. Other input parameters for it - projectName, compositeAppName, version, DeploymentIntentgroupName
+func (c *IntentClient) AddIntent(a Intent, p string, ca string, v string, di string) (Intent, error) {
+
+	//Check for the AddIntent already exists here.
+	res, err := c.GetIntent(a.MetaData.Name, p, ca, v, di)
+	if !reflect.DeepEqual(res, Intent{}) {
+		return Intent{}, pkgerrors.New("AppIntent already exists")
+	}
+
+	//Check if project exists
+	_, err = NewProjectClient().GetProject(p)
+	if err != nil {
+		return Intent{}, pkgerrors.New("Unable to find the project")
+	}
+
+	//check if compositeApp exists
+	_, err = NewCompositeAppClient().GetCompositeApp(ca, v, p)
+	if err != nil {
+		return Intent{}, pkgerrors.New("Unable to find the composite-app")
+	}
+
+	//check if DeploymentIntentGroup exists
+	_, err = NewDeploymentIntentGroupClient().GetDeploymentIntentGroup(di, p, ca, v)
+	if err != nil {
+		return Intent{}, pkgerrors.New("Unable to find the intent")
+	}
+
+	akey := IntentKey{
+		Name:                  a.MetaData.Name,
+		Project:               p,
+		CompositeApp:          ca,
+		Version:               v,
+		DeploymentIntentGroup: di,
+	}
+
+	err = db.DBconn.Insert(c.storeName, akey, nil, c.tagMetaData, a)
+	if err != nil {
+		return Intent{}, pkgerrors.Wrap(err, "Create DB entry error")
+	}
+	return a, nil
+}
+
+// GetIntent returns an Intent
+func (c *IntentClient) GetIntent(i string, p string, ca string, v string, di string) (Intent, error) {
+
+	k := IntentKey{
+		Name:                  i,
+		Project:               p,
+		CompositeApp:          ca,
+		Version:               v,
+		DeploymentIntentGroup: di,
+	}
+
+	result, err := db.DBconn.Find(c.storeName, k, c.tagMetaData)
+	if err != nil {
+		return Intent{}, pkgerrors.Wrap(err, "Get AppIntent error")
+	}
+
+	if result != nil {
+		a := Intent{}
+		err = db.DBconn.Unmarshal(result[0], &a)
+		if err != nil {
+			return Intent{}, pkgerrors.Wrap(err, "Unmarshalling  AppIntent")
+		}
+		return a, nil
+
+	}
+	return Intent{}, pkgerrors.New("Error getting AppIntent")
+}
+
+// DeleteIntent deletes a given intent tied to project, composite app and deployment intent group
+func (c IntentClient) DeleteIntent(i string, p string, ca string, v string, di string) error {
+	k := IntentKey{
+		Name:                  i,
+		Project:               p,
+		CompositeApp:          ca,
+		Version:               v,
+		DeploymentIntentGroup: di,
+	}
+
+	err := db.DBconn.Remove(c.storeName, k)
+	if err != nil {
+		return pkgerrors.Wrap(err, "Delete Project entry;")
+	}
+	return nil
+
+}
---
+++ src/orchestrator/pkg/module/deployment_intent_groups.go
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2020 Intel Corporation, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package module
+
+import (
+	"encoding/json"
+	"github.com/onap/multicloud-k8s/src/orchestrator/pkg/infra/db"
+	"reflect"
+
+	pkgerrors "github.com/pkg/errors"
+)
+
+// DeploymentIntentGroup shall have 2 fields - MetaData and Spec
+type DeploymentIntentGroup struct {
+	MetaData DepMetaData `json:"metadata"`
+	Spec     DepSpecData `json:"spec"`
+}
+
+// DepMetaData has Name, description, userdata1, userdata2
+type DepMetaData struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	UserData1   string `json:"userData1"`
+	UserData2   string `json:"userData2"`
+}
+
+// DepSpecData has profile, version, OverrideValuesObj
+type DepSpecData struct {
+	Profile           string           `json:"profile"`
+	Version           string           `json:"version"`
+	OverrideValuesObj []OverrideValues `json:"override-values"`
+}
+
+// OverrideValues has appName and ValuesObj
+type OverrideValues struct {
+	AppName   string   `json:"app-name"`
+	ValuesObj []Values `json:"values"`
+}
+
+// Values has ImageRepository
+type Values struct {
+	ImageRepository string `json:"imageRepository"`
+}
+
+// DeploymentIntentGroupManager is an interface which exposes the DeploymentIntentGroupManager functionality
+type DeploymentIntentGroupManager interface {
+	CreateDeploymentIntentGroup(d DeploymentIntentGroup, p string, ca string, v string) (DeploymentIntentGroup, error)
+	GetDeploymentIntentGroup(di string, p string, ca string, v string) (DeploymentIntentGroup, error)
+	DeleteDeploymentIntentGroup(di string, p string, ca string, v string) error
+}
+
+// DeploymentIntentGroupKey consists of Name of the deployment group, project name, CompositeApp name, CompositeApp version
+type DeploymentIntentGroupKey struct {
+	Name         string `json:"name"`
+	Project      string `json:"project"`
+	CompositeApp string `json:"compositeapp"`
+	Version      string `json:"version"`
+}
+
+// We will use json marshalling to convert to string to
+// preserve the underlying structure.
+func (dk DeploymentIntentGroupKey) String() string {
+	out, err := json.Marshal(dk)
+	if err != nil {
+		return ""
+	}
+	return string(out)
+}
+
+// DeploymentIntentGroupClient implements the DeploymentIntentGroupManager interface
+type DeploymentIntentGroupClient struct {
+	storeName   string
+	tagMetaData string
+}
+
+// NewDeploymentIntentGroupClient return an instance of DeploymentIntentGroupClient which implements DeploymentIntentGroupManager
+func NewDeploymentIntentGroupClient() *DeploymentIntentGroupClient {
+	return &DeploymentIntentGroupClient{
+		storeName:   "orchestrator",
+		tagMetaData: "deploymentintentgroup",
+	}
+}
+
+// CreateDeploymentIntentGroup creates an entry for a given  DeploymentIntentGroup in the database. Other Input parameters for it - projectName, compositeAppName, version
+func (c *DeploymentIntentGroupClient) CreateDeploymentIntentGroup(d DeploymentIntentGroup, p string, ca string,
+	v string) (DeploymentIntentGroup, error) {
+
+	res, err := c.GetDeploymentIntentGroup(d.MetaData.Name, p, ca, v)
+	if !reflect.DeepEqual(res, DeploymentIntentGroup{}) {
+		return DeploymentIntentGroup{}, pkgerrors.New("AppIntent already exists")
+	}
+
+	//Check if project exists
+	_, err = NewProjectClient().GetProject(p)
+	if err != nil {
+		return DeploymentIntentGroup{}, pkgerrors.New("Unable to find the project")
+	}
+
+	//check if compositeApp exists
+	_, err = NewCompositeAppClient().GetCompositeApp(ca, v, p)
+	if err != nil {
+		return DeploymentIntentGroup{}, pkgerrors.New("Unable to find the composite-app")
+	}
+
+	gkey := DeploymentIntentGroupKey{
+		Name:         d.MetaData.Name,
+		Project:      p,
+		CompositeApp: ca,
+		Version:      v,
+	}
+
+	err = db.DBconn.Insert(c.storeName, gkey, nil, c.tagMetaData, d)
+	if err != nil {
+		return DeploymentIntentGroup{}, pkgerrors.Wrap(err, "Create DB entry error")
+	}
+
+	return d, nil
+}
+
+// GetDeploymentIntentGroup returns the DeploymentIntentGroup with a given name, project, compositeApp and version of compositeApp
+func (c *DeploymentIntentGroupClient) GetDeploymentIntentGroup(di string, p string, ca string, v string) (DeploymentIntentGroup, error) {
+
+	key := DeploymentIntentGroupKey{
+		Name:         di,
+		Project:      p,
+		CompositeApp: ca,
+		Version:      v,
+	}
+
+	result, err := db.DBconn.Find(c.storeName, key, c.tagMetaData)
+	if err != nil {
+		return DeploymentIntentGroup{}, pkgerrors.Wrap(err, "Get DeploymentIntentGroup error")
+	}
+
+	if result != nil {
+		d := DeploymentIntentGroup{}
+		err = db.DBconn.Unmarshal(result[0], &d)
+		if err != nil {
+			return DeploymentIntentGroup{}, pkgerrors.Wrap(err, "Unmarshalling DeploymentIntentGroup")
+		}
+		return d, nil
+	}
+
+	return DeploymentIntentGroup{}, pkgerrors.New("Error getting DeploymentIntentGroup")
+
+}
+
+// DeleteDeploymentIntentGroup deletes a DeploymentIntentGroup
+func (c *DeploymentIntentGroupClient) DeleteDeploymentIntentGroup(di string, p string, ca string, v string) error {
+	k := DeploymentIntentGroupKey{
+		Name:         di,
+		Project:      p,
+		CompositeApp: ca,
+		Version:      v,
+	}
+
+	err := db.DBconn.Remove(c.storeName, k)
+	if err != nil {
+		return pkgerrors.Wrap(err, "Delete DeploymentIntentGroup entry;")
+	}
+	return nil
+
+}

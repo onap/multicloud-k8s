46ae88ed3d322c8c5276a46b63a49a6f203c034a
Marcus G K Williams <marcus.williams@intel.com>
2020-04-07 12:32:44 -0700

    Add hpacontroller framework
    Issue-ID: MULTICLOUD-1019
    Signed-off-by: Marcus G K Williams <marcus.williams@intel.com>
    Change-Id: I0f47ba2d6e40d966a27fbee681d71815cb997dfb

---
+++ src/hpacontroller/scripts/start-dev.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright 2020 Intel Corporation.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source _functions.sh
+
+#
+# Start from compiled binaries to foreground. This is usable for development use.
+#
+source /etc/environment
+opath="$(git rev-parse --show-toplevel)"/src/hpacontroller
+
+stop_all
+start_mongo
+start_etcd
+
+echo "Compiling source code"
+pushd $opath
+generate_config
+make all
+./hpacontroller
+popd

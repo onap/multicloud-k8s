ee5a41568738e0f6e55008c5c76c823b5d1b3fdb
Eric Multanen <eric.w.multanen@intel.com>
2020-04-16 12:03:15 -0700

    Add hpacontroller framework
    Issue-ID: MULTICLOUD-1019
    Signed-off-by: Marcus G K Williams <marcus.williams@intel.com>
    Signed-off-by: Eric Multanen <eric.w.multanen@intel.com>
    Change-Id: I0f47ba2d6e40d966a27fbee681d71815cb997dfb

---
+++ src/hpacontroller/pkg/infra/controllerdb/mock.go
@@ -0,0 +1,103 @@
+/*
+Copyright 2020 Intel Corporation.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllerdb
+
+import (
+	"encoding/json"
+	"fmt"
+	db "github.com/onap/multicloud-k8s/src/orchestrator/pkg/infra/db"
+
+	pkgerrors "github.com/pkg/errors"
+)
+
+type MockKey struct {
+	Key string
+}
+
+func (m MockKey) String() string {
+	return m.Key
+}
+
+//Creating an embedded interface via anonymous variable
+//This allows us to make mockDB satisfy the DatabaseConnection
+//interface even if we are not implementing all the methods in it
+type MockDB struct {
+	Store
+	Items map[string]map[string][]byte
+	Err   error
+}
+
+func (m *MockDB) HealthCheck() error {
+	return m.Err
+}
+
+func (m *MockDB) Create(table string, key db.Key, tag string, data interface{}) error {
+	return m.Err
+}
+
+func (m *MockDB) Insert(table string, key db.Key, query interface{}, tag string, data interface{}) error {
+	return m.Err
+}
+
+func (m *MockDB) Update(table string, key db.Key, tag string, data interface{}) error {
+	return m.Err
+}
+
+// MockDB uses simple JSON and not BSON
+func (m *MockDB) Unmarshal(inp []byte, out interface{}) error {
+	err := json.Unmarshal(inp, out)
+	if err != nil {
+		return pkgerrors.Wrap(err, "Unmarshaling json")
+	}
+	return nil
+}
+
+func (m *MockDB) Read(table string, key db.Key, tag string) ([]byte, error) {
+	if m.Err != nil {
+		return nil, m.Err
+	}
+
+	str := fmt.Sprintf("%v", key)
+	for k, v := range m.Items {
+		if k == str {
+			return v[tag], nil
+		}
+	}
+
+	return nil, m.Err
+}
+
+func (m *MockDB) Find(table string, key db.Key, tag string) ([][]byte, error) {
+	if m.Err != nil {
+		return nil, m.Err
+	}
+
+	str := fmt.Sprintf("%v", key)
+	for k, v := range m.Items {
+		if k == str {
+
+			return [][]byte{v[tag]}, nil
+		}
+	}
+
+	return nil, m.Err
+}
+
+func (m *MockDB) Delete(table string, key db.Key, tag string) error {
+	return m.Err
+}
+
+func (m *MockDB) Remove(table string, key db.Key) error {
+	return m.Err
+}

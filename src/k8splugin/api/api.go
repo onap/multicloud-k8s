9349580959fade616d0baff213a974c231b41df3
Konrad Bańka <k.banka@samsung.com>
2020-04-20 17:02:22 +0200

    Provide v1 endpoints accessible in backward compatible manner
    Depending on configfile option `preserve-v1-backward-compatibility`,
    start k8splugin in normal or backward compatible mode
    (listening on /, /v1, /v1/v1). By default, backward compatibility is
    turned off.
    
    Issue-ID: MULTICLOUD-1053
    Signed-off-by: Konrad Bańka <k.banka@samsung.com>
    Change-Id: I734fb7206db6fb20c9d108fc201207eecdc619e5

--- src/k8splugin/api/api.go
+++ src/k8splugin/api/api.go
@@ -15,11 +15,69 @@ package api
 
 import (
 	"github.com/onap/multicloud-k8s/src/k8splugin/internal/app"
+	"github.com/onap/multicloud-k8s/src/k8splugin/internal/config"
 	"github.com/onap/multicloud-k8s/src/k8splugin/internal/connection"
 	"github.com/onap/multicloud-k8s/src/k8splugin/internal/rb"
 
 	"github.com/gorilla/mux"
 )
+
+// Helper for returning all /v1/ API prefixes
+// according to context
+func yieldV1Prefixes() []string {
+	config := config.GetConfiguration()
+	if config.PreserveV1BackwardCompatibility == "true" {
+		return []string{"", "/v1", "/v1/v1"}
+	}
+	return []string{"/v1"}
+
+}
+
+// Helper for initializing all handlers
+func setupHandlers(
+	defClient rb.DefinitionManager,
+	profileClient rb.ProfileManager,
+	instClient app.InstanceManager,
+	configClient app.ConfigManager,
+	connectionClient connection.ConnectionManager,
+	templateClient rb.ConfigTemplateManager) (
+	instanceHandler,
+	brokerInstanceHandler,
+	connectionHandler,
+	rbDefinitionHandler,
+	rbProfileHandler,
+	rbTemplateHandler,
+	rbConfigHandler) {
+
+	if instClient == nil {
+		instClient = app.NewInstanceClient()
+	}
+	instHandler := instanceHandler{client: instClient}
+	brokerHandler := brokerInstanceHandler{client: instClient}
+	if connectionClient == nil {
+		connectionClient = connection.NewConnectionClient()
+	}
+	connectionHandler := connectionHandler{client: connectionClient}
+	if defClient == nil {
+		defClient = rb.NewDefinitionClient()
+	}
+	defHandler := rbDefinitionHandler{client: defClient}
+	if profileClient == nil {
+		profileClient = rb.NewProfileClient()
+	}
+	profileHandler := rbProfileHandler{client: profileClient}
+	if templateClient == nil {
+		templateClient = rb.NewConfigTemplateClient()
+	}
+	templateHandler := rbTemplateHandler{client: templateClient}
+	if configClient == nil {
+		configClient = app.NewConfigClient()
+	}
+	configHandler := rbConfigHandler{client: configClient}
+
+	return instHandler, brokerHandler, connectionHandler, defHandler,
+		profileHandler, templateHandler, configHandler
+}
 
 // NewRouter creates a router that registers the various urls that are supported
 func NewRouter(defClient rb.DefinitionManager,
@@ -31,92 +89,65 @@ func NewRouter(defClient rb.DefinitionManager,
 
 	router := mux.NewRouter()
 
-	// Setup Instance handler routes
-	if instClient == nil {
-		instClient = app.NewInstanceClient()
+	//Retrieve handlers from wrapper
+	instHandler, brokerHandler, connectionHandler,
+		defHandler, profileHandler, templateHandler,
+		configHandler := setupHandlers(defClient,
+		profileClient, instClient, configClient,
+		connectionClient, templateClient)
+
+	for _, prefix := range yieldV1Prefixes() {
+		subRouter := router.PathPrefix(prefix).Subrouter()
+
+		subRouter.HandleFunc("/healthcheck", healthCheckHandler).Methods("GET")
+
+		subRouter.HandleFunc("/instance", instHandler.createHandler).Methods("POST")
+		subRouter.HandleFunc("/instance", instHandler.listHandler).Methods("GET")
+		subRouter.HandleFunc("/instance", instHandler.listHandler).
+			Queries("rb-name", "{rb-name}",
+				"rb-version", "{rb-version}",
+				"profile-name", "{profile-name}").Methods("GET")
+
+		subRouter.HandleFunc("/instance/{instID}", instHandler.getHandler).Methods("GET")
+		subRouter.HandleFunc("/instance/{instID}/status", instHandler.statusHandler).Methods("GET")
+		subRouter.HandleFunc("/instance/{instID}", instHandler.deleteHandler).Methods("DELETE")
+		// (TODO): Fix update method
+		// subRouter.HandleFunc("/{vnfInstanceId}", UpdateHandler).Methods("PUT")
+
+		subRouter.HandleFunc("/{cloud-owner}/{cloud-region}/infra_workload", brokerHandler.createHandler).Methods("POST")
+		subRouter.HandleFunc("/{cloud-owner}/{cloud-region}/infra_workload/{instID}", brokerHandler.getHandler).Methods("GET")
+		subRouter.HandleFunc("/{cloud-owner}/{cloud-region}/infra_workload", brokerHandler.findHandler).Queries("name", "{name}").Methods("GET")
+		subRouter.HandleFunc("/{cloud-owner}/{cloud-region}/infra_workload/{instID}", brokerHandler.deleteHandler).Methods("DELETE")
+
+		subRouter.HandleFunc("/connectivity-info", connectionHandler.createHandler).Methods("POST")
+		subRouter.HandleFunc("/connectivity-info/{connname}", connectionHandler.getHandler).Methods("GET")
+		subRouter.HandleFunc("/connectivity-info/{connname}", connectionHandler.deleteHandler).Methods("DELETE")
+
+		resRouter := subRouter.PathPrefix("/rb").Subrouter()
+		resRouter.HandleFunc("/definition", defHandler.createHandler).Methods("POST")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/content", defHandler.uploadHandler).Methods("POST")
+		resRouter.HandleFunc("/definition/{rbname}", defHandler.listVersionsHandler).Methods("GET")
+		resRouter.HandleFunc("/definition", defHandler.listAllHandler).Methods("GET")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}", defHandler.getHandler).Methods("GET")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}", defHandler.deleteHandler).Methods("DELETE")
+
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile", profileHandler.createHandler).Methods("POST")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile", profileHandler.listHandler).Methods("GET")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}/content", profileHandler.uploadHandler).Methods("POST")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}", profileHandler.getHandler).Methods("GET")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}", profileHandler.deleteHandler).Methods("DELETE")
+
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/config-template", templateHandler.createHandler).Methods("POST")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/config-template/{tname}/content", templateHandler.uploadHandler).Methods("POST")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/config-template/{tname}", templateHandler.getHandler).Methods("GET")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/config-template/{tname}", templateHandler.deleteHandler).Methods("DELETE")
+
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}/config", configHandler.createHandler).Methods("POST")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}/config/{cfgname}", configHandler.getHandler).Methods("GET")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}/config/{cfgname}", configHandler.updateHandler).Methods("PUT")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}/config/{cfgname}", configHandler.deleteHandler).Methods("DELETE")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}/config/rollback", configHandler.rollbackHandler).Methods("POST")
+		resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}/config/tagit", configHandler.tagitHandler).Methods("POST")
 	}
-	instHandler := instanceHandler{client: instClient}
-	instRouter := router.PathPrefix("/v1").Subrouter()
-	instRouter.HandleFunc("/instance", instHandler.createHandler).Methods("POST")
-	instRouter.HandleFunc("/instance", instHandler.listHandler).Methods("GET")
-	// Match rb-names, versions or profiles
-	instRouter.HandleFunc("/instance", instHandler.listHandler).
-		Queries("rb-name", "{rb-name}",
-			"rb-version", "{rb-version}",
-			"profile-name", "{profile-name}").Methods("GET")
-
-	instRouter.HandleFunc("/instance/{instID}", instHandler.getHandler).Methods("GET")
-	instRouter.HandleFunc("/instance/{instID}/status", instHandler.statusHandler).Methods("GET")
-	instRouter.HandleFunc("/instance/{instID}", instHandler.deleteHandler).Methods("DELETE")
-	// (TODO): Fix update method
-	// instRouter.HandleFunc("/{vnfInstanceId}", UpdateHandler).Methods("PUT")
-
-	//Setup the broker handler here
-	//Use the base router without any path prefixes
-	brokerHandler := brokerInstanceHandler{client: instClient}
-	brokerRouter := router.PathPrefix("/v1").Subrouter()
-	brokerRouter.HandleFunc("/{cloud-owner}/{cloud-region}/infra_workload", brokerHandler.createHandler).Methods("POST")
-	brokerRouter.HandleFunc("/{cloud-owner}/{cloud-region}/infra_workload/{instID}", brokerHandler.getHandler).Methods("GET")
-	brokerRouter.HandleFunc("/{cloud-owner}/{cloud-region}/infra_workload", brokerHandler.findHandler).Queries("name", "{name}").Methods("GET")
-	brokerRouter.HandleFunc("/{cloud-owner}/{cloud-region}/infra_workload/{instID}", brokerHandler.deleteHandler).Methods("DELETE")
-
-	//Setup the connectivity api handler here
-	if connectionClient == nil {
-		connectionClient = connection.NewConnectionClient()
-	}
-	connectionHandler := connectionHandler{client: connectionClient}
-	instRouter.HandleFunc("/connectivity-info", connectionHandler.createHandler).Methods("POST")
-	instRouter.HandleFunc("/connectivity-info/{connname}", connectionHandler.getHandler).Methods("GET")
-	instRouter.HandleFunc("/connectivity-info/{connname}", connectionHandler.deleteHandler).Methods("DELETE")
-
-	//Setup resource bundle definition routes
-	if defClient == nil {
-		defClient = rb.NewDefinitionClient()
-	}
-	defHandler := rbDefinitionHandler{client: defClient}
-	resRouter := router.PathPrefix("/v1/rb").Subrouter()
-	resRouter.HandleFunc("/definition", defHandler.createHandler).Methods("POST")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/content", defHandler.uploadHandler).Methods("POST")
-	resRouter.HandleFunc("/definition/{rbname}", defHandler.listVersionsHandler).Methods("GET")
-	resRouter.HandleFunc("/definition", defHandler.listAllHandler).Methods("GET")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}", defHandler.getHandler).Methods("GET")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}", defHandler.deleteHandler).Methods("DELETE")
-
-	//Setup resource bundle profile routes
-	if profileClient == nil {
-		profileClient = rb.NewProfileClient()
-	}
-	profileHandler := rbProfileHandler{client: profileClient}
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile", profileHandler.createHandler).Methods("POST")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile", profileHandler.listHandler).Methods("GET")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}/content", profileHandler.uploadHandler).Methods("POST")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}", profileHandler.getHandler).Methods("GET")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}", profileHandler.deleteHandler).Methods("DELETE")
-
-	// Config Template
-	if templateClient == nil {
-		templateClient = rb.NewConfigTemplateClient()
-	}
-	templateHandler := rbTemplateHandler{client: templateClient}
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/config-template", templateHandler.createHandler).Methods("POST")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/config-template/{tname}/content", templateHandler.uploadHandler).Methods("POST")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/config-template/{tname}", templateHandler.getHandler).Methods("GET")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/config-template/{tname}", templateHandler.deleteHandler).Methods("DELETE")
-
-	// Config value
-	if configClient == nil {
-		configClient = app.NewConfigClient()
-	}
-	configHandler := rbConfigHandler{client: configClient}
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}/config", configHandler.createHandler).Methods("POST")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}/config/{cfgname}", configHandler.getHandler).Methods("GET")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}/config/{cfgname}", configHandler.updateHandler).Methods("PUT")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}/config/{cfgname}", configHandler.deleteHandler).Methods("DELETE")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}/config/rollback", configHandler.rollbackHandler).Methods("POST")
-	resRouter.HandleFunc("/definition/{rbname}/{rbversion}/profile/{prname}/config/tagit", configHandler.tagitHandler).Methods("POST")
-
-	// Add healthcheck path
-	instRouter.HandleFunc("/healthcheck", healthCheckHandler).Methods("GET")
-
 	return router
 }
--- src/k8splugin/api/healthcheckhandler_test.go
+++ src/k8splugin/api/healthcheckhandler_test.go
@@ -21,6 +21,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/onap/multicloud-k8s/src/k8splugin/internal/config"
 	"github.com/onap/multicloud-k8s/src/k8splugin/internal/db"
 
 	pkgerrors "github.com/pkg/errors"
@@ -30,29 +31,51 @@ import (
 // and its backing database
 func TestHealthCheckHandler(t *testing.T) {
 
-	t.Run("OK HealthCheck", func(t *testing.T) {
-		db.DBconn = &db.MockDB{
-			Err: nil,
-		}
-		request := httptest.NewRequest("GET", "/v1/healthcheck", nil)
-		resp := executeRequest(request, NewRouter(nil, nil, nil, nil, nil, nil))
+	for _, prefix := range yieldV1Prefixes() {
+		for _, backCompat := range []string{"false", "true"} {
+			t.Run("OK HealthCheck ("+backCompat+prefix+")", func(t *testing.T) {
+				config.SetConfigValue("PreserveV1BackwardCompatibility", backCompat)
+				var expectedCode int
+				if backCompat == "false" && prefix != "/v1" {
+					expectedCode = http.StatusNotFound
+				} else {
+					expectedCode = http.StatusOK
+				}
+				db.DBconn = &db.MockDB{
+					Err: nil,
+				}
+				request := httptest.NewRequest("GET", prefix+"/healthcheck", nil)
+				resp := executeRequest(request, NewRouter(nil, nil, nil, nil, nil, nil))
 
-		//Check returned code
-		if resp.StatusCode != http.StatusOK {
-			t.Fatalf("Expected %d; Got: %d", http.StatusOK, resp.StatusCode)
+				//Check returned code
+				if resp.StatusCode != expectedCode {
+					t.Fatalf("Expected %d; Got: %d", expectedCode, resp.StatusCode)
+				}
+			})
 		}
-	})
+	}
 
-	t.Run("FAILED HealthCheck", func(t *testing.T) {
-		db.DBconn = &db.MockDB{
-			Err: pkgerrors.New("Runtime Error in DB"),
-		}
-		request := httptest.NewRequest("GET", "/v1/healthcheck", nil)
-		resp := executeRequest(request, NewRouter(nil, nil, nil, nil, nil, nil))
+	for _, prefix := range yieldV1Prefixes() {
+		for _, backCompat := range []string{"false", "true"} {
+			t.Run("FAILED HealthCheck ("+backCompat+prefix+")", func(t *testing.T) {
+				config.SetConfigValue("PreserveV1BackwardCompatibility", backCompat)
+				var expectedCode int
+				if backCompat == "false" && prefix != "/v1" {
+					expectedCode = http.StatusNotFound
+				} else {
+					expectedCode = http.StatusInternalServerError
+				}
+				db.DBconn = &db.MockDB{
+					Err: pkgerrors.New("Runtime Error in DB"),
+				}
+				request := httptest.NewRequest("GET", prefix+"/healthcheck", nil)
+				resp := executeRequest(request, NewRouter(nil, nil, nil, nil, nil, nil))
 
-		//Check returned code
-		if resp.StatusCode != http.StatusInternalServerError {
-			t.Fatalf("Expected %d; Got: %d", http.StatusInternalServerError, resp.StatusCode)
+				//Check returned code
+				if resp.StatusCode != expectedCode {
+					t.Fatalf("Expected %d; Got: %d", expectedCode, resp.StatusCode)
+				}
+			})
 		}
-	})
+	}
 }

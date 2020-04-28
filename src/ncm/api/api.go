4100bfb764c40e2a788d28691b0f891e2ae86b74
Eric Multanen <eric.w.multanen@intel.com>
2020-04-27 17:33:25 -0700

    Add API support for network chain intent
    Adds API for managing Network Chain intents for
    composite applications.
    
    Issue-ID: MULTICLOUD-1029
    Signed-off-by: Eric Multanen <eric.w.multanen@intel.com>
    Change-Id: Ie3d4e8f1e3179397d86829d951e2bb877597b38d

--- src/ncm/api/api.go
+++ src/ncm/api/api.go
@@ -72,6 +72,13 @@ func setClient(client, testClient interface{}) interface{} {
 				return c
 			}
 		}
+	case *moduleLib.ChainClient:
+		if testClient != nil && reflect.TypeOf(testClient).Implements(reflect.TypeOf((*moduleLib.ChainManager)(nil)).Elem()) {
+			c, ok := testClient.(moduleLib.ChainManager)
+			if ok {
+				return c
+			}
+		}
 	default:
 		fmt.Printf("unknown type %T\n", cl)
 	}
@@ -154,6 +161,15 @@ func NewRouter(testClient interface{}) *mux.Router {
 	router.HandleFunc("/projects/{project}/composite-apps/{composite-app-name}/{version}/network-controller-intent/{net-control-intent}/workload-intents/{workload-intent}/interfaces/{name}", workloadifintentHandler.putHandler).Methods("PUT")
 	router.HandleFunc("/projects/{project}/composite-apps/{composite-app-name}/{version}/network-controller-intent/{net-control-intent}/workload-intents/{workload-intent}/interfaces/{name}", workloadifintentHandler.getHandler).Methods("GET")
 	router.HandleFunc("/projects/{project}/composite-apps/{composite-app-name}/{version}/network-controller-intent/{net-control-intent}/workload-intents/{workload-intent}/interfaces/{name}", workloadifintentHandler.deleteHandler).Methods("DELETE")
+
+	chainHandler := chainHandler{
+		client: setClient(moduleClient.Chain, testClient).(moduleLib.ChainManager),
+	}
+	router.HandleFunc("/projects/{project}/composite-apps/{composite-app-name}/{version}/network-controller-intent/{net-control-intent}/network-chains", chainHandler.createHandler).Methods("POST")
+	router.HandleFunc("/projects/{project}/composite-apps/{composite-app-name}/{version}/network-controller-intent/{net-control-intent}/network-chains", chainHandler.getHandler).Methods("GET")
+	router.HandleFunc("/projects/{project}/composite-apps/{composite-app-name}/{version}/network-controller-intent/{net-control-intent}/network-chains/{name}", chainHandler.putHandler).Methods("PUT")
+	router.HandleFunc("/projects/{project}/composite-apps/{composite-app-name}/{version}/network-controller-intent/{net-control-intent}/network-chains/{name}", chainHandler.getHandler).Methods("GET")
+	router.HandleFunc("/projects/{project}/composite-apps/{composite-app-name}/{version}/network-controller-intent/{net-control-intent}/network-chains/{name}", chainHandler.deleteHandler).Methods("DELETE")
 
 	return router
 }

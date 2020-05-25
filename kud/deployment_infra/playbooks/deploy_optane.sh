ac3356c8920fe3a62989480e3162bb7e47f92dc7
Chen, Tingjie <tingjie.chen@intel.com>
2020-05-25 14:26:26 +0800

    Add support for pmem-csi plugin and e2e test
    Issue-ID: MULTICLOUD-1046
    Change-Id: I1853e071a99702c5e6f7ba9ca819746576fd0aca
    Signed-off-by: Chen, Tingjie <tingjie.chen@intel.com>

---
+++ kud/deployment_infra/playbooks/deploy_optane.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+work_path="$(dirname -- "$(readlink -f -- "$0")")"
+ndctl_region=`ndctl list -R`
+
+if [ -n $ndctl_region ]; then
+    echo "No Optane Hardware!"
+else
+    echo "Optane Plugin start .."
+    /usr/local/bin/kubectl apply -f $work_path/pmem-csi-lvm.yaml
+fi

8f5fbc9996860c79c9ee24ee60adec3ac15cf412
Eric Multanen <eric.w.multanen@intel.com>
2020-06-23 12:39:26 -0700

    Add composite vFW demo
    Add demonstration files and test scripts to show the vFW
    use case as a composite application using network intents
    and ovnaction intents to deploy to multiple clusters.
    
    Issue-ID: MULTICLOUD-1095
    Signed-off-by: Eric Multanen <eric.w.multanen@intel.com>
    Change-Id: I74b837f9f97747f1eefffbcd105a6630a7b3a374

---
+++ deployments/kubernetes/deletestuff.sh
@@ -0,0 +1,15 @@
+kubectl -n onap4k8s delete deployment clm
+kubectl -n onap4k8s delete deployment orchestrator
+kubectl -n onap4k8s delete deployment ncm
+kubectl -n onap4k8s delete deployment ovnaction
+kubectl -n onap4k8s delete deployment rsync
+kubectl -n onap4k8s delete service clm
+kubectl -n onap4k8s delete service orchestrator
+kubectl -n onap4k8s delete service ncm
+kubectl -n onap4k8s delete service ovnaction
+kubectl -n onap4k8s delete service rsync
+kubectl -n onap4k8s delete configmap clm
+kubectl -n onap4k8s delete configmap orchestrator
+kubectl -n onap4k8s delete configmap ncm
+kubectl -n onap4k8s delete configmap ovnaction
+kubectl -n onap4k8s delete configmap rsync

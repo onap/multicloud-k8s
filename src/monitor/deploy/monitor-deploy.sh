725aa2a832e43853878cf66340e0fe4bed323ddf
Eric Multanen <eric.w.multanen@intel.com>
2020-07-01 23:30:49 -0700

    Adds composite app status update and query
    This patch provides the basic framework for supporting
    monitoring of composite application resources in clusters.
    
    1. Updates to the monitor files for use with v2.
    2. Invokes the Watcher process per cluster/app when the
       app is instantiated.
    3. Adds a ResourceBundleState CR resource to the cluster/app
       so that monitor will be able to update status to it.
    4. Watcher updates appropriate appcontext status object
       when updates are made in clusters by monitor
    5. Update appcontext library to define a status handle
       and object at the app/cluster level
    6. Labels resources with an appropriate tracking label
       to coordinate with the ResourceBundleState CR
    
    Issue-ID: MULTICLOUD-1042
    Signed-off-by: Eric Multanen <eric.w.multanen@intel.com>
    Change-Id: If007c1fd86ca7a65bb941d1776cfd2d3afed766b

---
+++ src/monitor/deploy/monitor-deploy.sh
@@ -0,0 +1,6 @@
+kubectl apply -f role.yaml
+kubectl apply -f cluster_role.yaml
+kubectl apply -f role_binding.yaml
+kubectl apply -f clusterrole_binding.yaml
+kubectl apply -f service_account.yaml
+kubectl apply -f operator.yaml

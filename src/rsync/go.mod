5cae2742bcb7ef66faad139b2318677810316b9a
Ritu Sood <ritu.sood@intel.com>
2020-07-06 12:13:42 -0700

    DO NOT MERGE - EXPERIMENTAL
    Issue-ID: MULTICLOUD-1005
    Signed-off-by: Ritu Sood <ritu.sood@intel.com>
    Change-Id: I4c2537cb74bd4d24a409cc1f0b7f9ee0875a4e39

--- src/rsync/go.mod
+++ src/rsync/go.mod
@@ -3,27 +3,36 @@ module github.com/onap/multicloud-k8s/src/rsync
 go 1.13
 
 require (
+
+	//client
+	github.com/evanphx/json-patch v4.5.0+incompatible // indirect
 	github.com/golang/protobuf v1.4.1
 	github.com/googleapis/gnostic v0.4.0
 	github.com/grpc-ecosystem/go-grpc-middleware v1.0.1-0.20190118093823-f849b5445de4 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.9.5 // indirect
+	github.com/jonboulle/clockwork v0.1.0
 	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/onap/multicloud-k8s/src/clm v0.0.0-00010101000000-000000000000
 	github.com/onap/multicloud-k8s/src/orchestrator v0.0.0-20200601021239-7959bd4c6fd4
+	//github.com/onap/multicloud-k8s/src/orchestrator v0.0.0-20200601021239-7959bd4c6fd4
+	github.com/pkg/errors v0.8.1
 	go.etcd.io/bbolt v1.3.3 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
 	google.golang.org/grpc v1.27.1
+	k8s.io/api v0.17.3
+	k8s.io/apiextensions-apiserver v0.17.3
+	k8s.io/apimachinery v0.17.3
+	k8s.io/cli-runtime v0.17.3
+	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
+	k8s.io/kube-openapi v0.0.0-20191107075043-30be4d16710a
+	k8s.io/kubectl v0.17.3
 	k8s.io/kubernetes v1.14.1
 )
 
 replace (
 	github.com/onap/multicloud-k8s/src/clm => ../clm
-	github.com/onap/multicloud-k8s/src/orchestrator => ../orchestrator
+	//github.com/onap/multicloud-k8s/src/orchestrator => ../orchestrator
 	github.com/onap/multicloud-k8s/src/rsync => ../rsync
-	k8s.io/api => k8s.io/api v0.0.0-20190409021203-6e4e0e4f393b
-	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.0.0-20190409022649-727a075fdec8
-	k8s.io/apimachinery => k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
-	k8s.io/apiserver => k8s.io/apiserver v0.0.0-20190409021813-1ec86e4da56c
-	k8s.io/cli-runtime => k8s.io/cli-runtime v0.0.0-20190409023024-d644b00f3b79
-	k8s.io/client-go => k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
-	k8s.io/cloud-provider => k8s.io/cloud-provider v0.0.0-20190409023720-1bc0c81fa51d
+	k8s.io/client-go => k8s.io/client-go v0.17.3
 )
--- src/rsync/pkg/app/client.go
+++ src/rsync/pkg/app/client.go
@@ -14,22 +14,18 @@ limitations under the License.
 package app
 
 import (
+	"encoding/base64"
 	"os"
 	"strings"
-	"time"
-	"encoding/base64"
 
 	pkgerrors "github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/client-go/discovery/cached/disk"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/restmapper"
-	"k8s.io/client-go/tools/clientcmd"
 
 	"github.com/onap/multicloud-k8s/src/clm/pkg/cluster"
 )
-
 
 // KubernetesClient encapsulates the different clients' interfaces
 // we need when interacting with a Kubernetes cluster
@@ -64,67 +60,45 @@ func (k *KubernetesClient) getKubeConfig(clustername string, id string) ([]byte,
 	return dec, nil
 }
 
-// init loads the Kubernetes configuation values stored into the local configuration file
-func (k *KubernetesClient) Init(clustername string, iid string) error {
-	if clustername == "" {
-		return pkgerrors.New("Cloudregion is empty")
+const basePath string = "/tmp/rsync/"
+
+func (k *KubernetesClient) GetKubeConfigFile(clustername string, id string) (string, error) {
+
+	if !strings.Contains(clustername, "+") {
+		return "", pkgerrors.New("Not a valid cluster name")
 	}
-
-	if iid == "" {
-		return pkgerrors.New("Instance ID is empty")
+	strs := strings.Split(clustername, "+")
+	if len(strs) != 2 {
+		return "", pkgerrors.New("Not a valid cluster name")
 	}
-
-	k.instanceID = iid
-
-	configData, err := k.getKubeConfig(clustername, iid)
+	kubeConfig, err := cluster.NewClusterClient().GetClusterContent(strs[0], strs[1])
 	if err != nil {
-		return pkgerrors.Wrap(err, "Get kubeconfig file")
+		return "", pkgerrors.New("Get kubeconfig failed")
 	}
 
-	config, err := clientcmd.RESTConfigFromKubeConfig(configData)
+	var kubeConfigPath string = basePath + id + "/" + clustername + "/"
+
+	if _, err := os.Stat(kubeConfigPath); os.IsNotExist(err) {
+		err = os.MkdirAll(kubeConfigPath, 0755)
+		if err != nil {
+			return "", err
+		}
+	}
+	kubeConfigPath = kubeConfigPath + "config"
+
+	f, err := os.Create(kubeConfigPath)
+	defer f.Close()
 	if err != nil {
-		return pkgerrors.Wrap(err, "setConfig: Build config from flags raised an error")
+		return "", err
 	}
-
-	k.clientSet, err = kubernetes.NewForConfig(config)
+	dec, err := base64.StdEncoding.DecodeString(kubeConfig.Kubeconfig)
 	if err != nil {
-		return err
+		return "", err
 	}
-
-	k.dynamicClient, err = dynamic.NewForConfig(config)
+	_, err = f.Write(dec)
 	if err != nil {
-		return pkgerrors.Wrap(err, "Creating dynamic client")
+		return "", err
 	}
 
-	k.discoverClient, err = disk.NewCachedDiscoveryClientForConfig(config, os.TempDir(), "", 10*time.Minute)
-	if err != nil {
-		return pkgerrors.Wrap(err, "Creating discovery client")
-	}
-
-	k.restMapper = restmapper.NewDeferredDiscoveryRESTMapper(k.discoverClient)
-
-	return nil
-}
-
-//GetMapper returns the RESTMapper that was created for this client
-func (k *KubernetesClient) GetMapper() meta.RESTMapper {
-	return k.restMapper
-}
-
-//GetDynamicClient returns the dynamic client that is needed for
-//unstructured REST calls to the apiserver
-func (k *KubernetesClient) GetDynamicClient() dynamic.Interface {
-	return k.dynamicClient
-}
-
-// GetStandardClient returns the standard client that can be used to handle
-// standard kubernetes kinds
-func (k *KubernetesClient) GetStandardClient() kubernetes.Interface {
-	return k.clientSet
-}
-
-//GetInstanceID returns the instanceID that is injected into all the
-//resources created by the plugin
-func (k *KubernetesClient) GetInstanceID() string {
-	return k.instanceID
+	return kubeConfigPath, nil
 }

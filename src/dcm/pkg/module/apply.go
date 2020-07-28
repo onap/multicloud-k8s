c21d5ba4e3e111034d6b4cdd6ad0c172433be41c
Igor DC <igor.duarte.cardoso@intel.com>
2020-07-28 02:58:07 +0000

    [WIP] Implement missing DCM features
    - Now calling rsync over gRPC
    - Implemented Terminate handler (/terminate)
    - Tested that mongodb/etcd/k8s workflow works
    - But only testing for 1 k8s resource: namespace
    
    Known issue: limited to 1 cluster at the moment.
    
    Issue-ID: MULTICLOUD-1143
    Signed-off-by: Igor D.C <igor.duarte.cardoso@intel.com>
    Change-Id: Ie0bc18f366f7f95045f1b50e2e316b3e5d22e77e

--- src/dcm/pkg/module/apply.go
+++ src/dcm/pkg/module/apply.go
@@ -27,7 +27,10 @@ import (
 	"fmt"
 	"strings"
 
+	clusterPkg "github.com/onap/multicloud-k8s/src/clm/pkg/cluster"
 	"github.com/onap/multicloud-k8s/src/orchestrator/pkg/appcontext"
+	"github.com/onap/multicloud-k8s/src/orchestrator/pkg/grpc/installappclient"
+	"github.com/onap/multicloud-k8s/src/orchestrator/pkg/infra/db"
 	log "github.com/onap/multicloud-k8s/src/orchestrator/pkg/infra/logutils"
 	pkgerrors "github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
@@ -226,7 +229,6 @@ func createUserCSR(logicalcloud LogicalCloud) (string, error) {
 // kubectl get csr lc1-user-cert -o jsonpath='{.status.certificate}' | base64 --decode > user.crt
 // kubectl config set-credentials user --client-certificate=<user.crt>  --client-key=<user.key>
 // kubectl config set-context user-context --cluster=cluster-name --namespace=lc1 --user=user
-
 func CreateEtcdContext(logicalcloud LogicalCloud, clusterList []Cluster,
 	quotaList []Quota) error {
 
@@ -235,10 +237,10 @@ func CreateEtcdContext(logicalcloud LogicalCloud, clusterList []Cluster,
 
 	//Resource Names
 	namespaceName := strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "+namespace"}, "")
-	roleName := strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "+role"}, "")
-	roleBindingName := strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "+roleBinding"}, "")
-	quotaName := strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "+quota"}, "")
-	csrName := strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "+CertificateSigningRequest"}, "")
+	// roleName := strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "+role"}, "")
+	// roleBindingName := strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "+roleBinding"}, "")
+	// quotaName := strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "+quota"}, "")
+	// csrName := strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "+CertificateSigningRequest"}, "")
 
 	// Get resources to be added
 	namespace, err := createNamespace(logicalcloud)
@@ -246,25 +248,25 @@ func CreateEtcdContext(logicalcloud LogicalCloud, clusterList []Cluster,
 		return pkgerrors.Wrap(err, "Error Creating Namespace YAML for logical cloud")
 	}
 
-	role, err := createRole(logicalcloud)
-	if err != nil {
-		return pkgerrors.Wrap(err, "Error Creating Role YAML for logical cloud")
-	}
+	// role, err := createRole(logicalcloud)
+	// if err != nil {
+	// 	return pkgerrors.Wrap(err, "Error Creating Role YAML for logical cloud")
+	// }
 
-	roleBinding, err := createRoleBinding(logicalcloud)
-	if err != nil {
-		return pkgerrors.Wrap(err, "Error Creating RoleBinding YAML for logical cloud")
-	}
+	// roleBinding, err := createRoleBinding(logicalcloud)
+	// if err != nil {
+	// 	return pkgerrors.Wrap(err, "Error Creating RoleBinding YAML for logical cloud")
+	// }
 
-	quota, err := createQuota(quotaList, logicalcloud.Specification.NameSpace)
-	if err != nil {
-		return pkgerrors.Wrap(err, "Error Creating Quota YAML for logical cloud")
-	}
+	// quota, err := createQuota(quotaList, logicalcloud.Specification.NameSpace)
+	// if err != nil {
+	// 	return pkgerrors.Wrap(err, "Error Creating Quota YAML for logical cloud")
+	// }
 
-	csr, err := createUserCSR(logicalcloud)
-	if err != nil {
-		return pkgerrors.Wrap(err, "Error Creating User CSR for logical cloud")
-	}
+	// csr, err := createUserCSR(logicalcloud)
+	// if err != nil {
+	// 	return pkgerrors.Wrap(err, "Error Creating User CSR for logical cloud")
+	// }
 
 	context := appcontext.AppContext{}
 	ctxVal, err := context.InitAppContext()
@@ -321,71 +323,82 @@ func CreateEtcdContext(logicalcloud LogicalCloud, clusterList []Cluster,
 			return pkgerrors.Wrap(err, "Error adding Namespace Resource to AppContext")
 		}
 
-		// Add csr resource to each cluster
-		_, err = context.AddResource(clusterHandle, csrName, csr)
-		if err != nil {
-			cleanuperr := context.DeleteCompositeApp()
-			if cleanuperr != nil {
-				log.Warn("Error cleaning AppContext after add CSR resource failure", log.Fields{
-					"cluster-provider": cluster.Specification.ClusterProvider,
-					"cluster":          cluster.Specification.ClusterName,
-					"logical-cloud":    logicalCloudName,
-				})
-			}
-			return pkgerrors.Wrap(err, "Error adding CSR Resource to AppContext")
-		}
+		// // Add csr resource to each cluster
+		// _, err = context.AddResource(clusterHandle, csrName, csr)
+		// if err != nil {
+		// 	cleanuperr := context.DeleteCompositeApp()
+		// 	if cleanuperr != nil {
+		// 		log.Warn("Error cleaning AppContext after add CSR resource failure", log.Fields{
+		// 			"cluster-provider": cluster.Specification.ClusterProvider,
+		// 			"cluster":          cluster.Specification.ClusterName,
+		// 			"logical-cloud":    logicalCloudName,
+		// 		})
+		// 	}
+		// 	return pkgerrors.Wrap(err, "Error adding CSR Resource to AppContext")
+		// }
 
-		// Add Role resource to each cluster
-		_, err = context.AddResource(clusterHandle, roleName, role)
-		if err != nil {
-			cleanuperr := context.DeleteCompositeApp()
-			if cleanuperr != nil {
-				log.Warn("Error cleaning AppContext after add role resource failure", log.Fields{
-					"cluster-provider": cluster.Specification.ClusterProvider,
-					"cluster":          cluster.Specification.ClusterName,
-					"logical-cloud":    logicalCloudName,
-				})
-			}
-			return pkgerrors.Wrap(err, "Error adding role Resource to AppContext")
-		}
+		// // Add Role resource to each cluster
+		// _, err = context.AddResource(clusterHandle, roleName, role)
+		// if err != nil {
+		// 	cleanuperr := context.DeleteCompositeApp()
+		// 	if cleanuperr != nil {
+		// 		log.Warn("Error cleaning AppContext after add role resource failure", log.Fields{
+		// 			"cluster-provider": cluster.Specification.ClusterProvider,
+		// 			"cluster":          cluster.Specification.ClusterName,
+		// 			"logical-cloud":    logicalCloudName,
+		// 		})
+		// 	}
+		// 	return pkgerrors.Wrap(err, "Error adding role Resource to AppContext")
+		// }
 
-		// Add RoleBinding resource to each cluster
-		_, err = context.AddResource(clusterHandle, roleBindingName, roleBinding)
-		if err != nil {
-			cleanuperr := context.DeleteCompositeApp()
-			if cleanuperr != nil {
-				log.Warn("Error cleaning AppContext after add roleBinding resource failure", log.Fields{
-					"cluster-provider": cluster.Specification.ClusterProvider,
-					"cluster":          cluster.Specification.ClusterName,
-					"logical-cloud":    logicalCloudName,
-				})
-			}
-			return pkgerrors.Wrap(err, "Error adding roleBinding Resource to AppContext")
-		}
+		// // Add RoleBinding resource to each cluster
+		// _, err = context.AddResource(clusterHandle, roleBindingName, roleBinding)
+		// if err != nil {
+		// 	cleanuperr := context.DeleteCompositeApp()
+		// 	if cleanuperr != nil {
+		// 		log.Warn("Error cleaning AppContext after add roleBinding resource failure", log.Fields{
+		// 			"cluster-provider": cluster.Specification.ClusterProvider,
+		// 			"cluster":          cluster.Specification.ClusterName,
+		// 			"logical-cloud":    logicalCloudName,
+		// 		})
+		// 	}
+		// 	return pkgerrors.Wrap(err, "Error adding roleBinding Resource to AppContext")
+		// }
 
-		// Add quota resource to each cluster
-		_, err = context.AddResource(clusterHandle, quotaName, quota)
-		if err != nil {
-			cleanuperr := context.DeleteCompositeApp()
-			if cleanuperr != nil {
-				log.Warn("Error cleaning AppContext after add quota resource failure", log.Fields{
-					"cluster-provider": cluster.Specification.ClusterProvider,
-					"cluster":          cluster.Specification.ClusterName,
-					"logical-cloud":    logicalCloudName,
-				})
-			}
-			return pkgerrors.Wrap(err, "Error adding quota Resource to AppContext")
-		}
+		// // Add quota resource to each cluster
+		// _, err = context.AddResource(clusterHandle, quotaName, quota)
+		// if err != nil {
+		// 	cleanuperr := context.DeleteCompositeApp()
+		// 	if cleanuperr != nil {
+		// 		log.Warn("Error cleaning AppContext after add quota resource failure", log.Fields{
+		// 			"cluster-provider": cluster.Specification.ClusterProvider,
+		// 			"cluster":          cluster.Specification.ClusterName,
+		// 			"logical-cloud":    logicalCloudName,
+		// 		})
+		// 	}
+		// 	return pkgerrors.Wrap(err, "Error adding quota Resource to AppContext")
+		// }
 
 		// Add Resource Order and Resource Dependency
-		resOrder, err := json.Marshal(map[string][]string{"resorder": []string{namespaceName, quotaName, csrName, roleName, roleBindingName}})
+		resOrder, err := json.Marshal(map[string][]string{"resorder": []string{namespaceName}})
 		if err != nil {
 			return pkgerrors.Wrap(err, "Error creating resource order JSON")
 		}
+		resDependency, err := json.Marshal(map[string]map[string]string{"resdependency": map[string]string{namespaceName: "go"}})
+		// resOrder, err := json.Marshal(map[string][]string{"resorder": []string{namespaceName, quotaName, csrName, roleName, roleBindingName}})
+		// if err != nil {
+		// 	return pkgerrors.Wrap(err, "Error creating resource order JSON")
+		// }
+		// resDependency, err := json.Marshal(map[string]map[string]string{"resdependency": map[string]string{namespaceName: "go",
+		// 	quotaName: strings.Join([]string{"wait on ", namespaceName}, ""), csrName: strings.Join([]string{"wait on ", quotaName}, ""),
+		// 	roleName: strings.Join([]string{"wait on ", csrName}, ""), roleBindingName: strings.Join([]string{"wait on ", roleName}, "")}})
 
-		resDependency, err := json.Marshal(map[string]map[string]string{"resdependency": map[string]string{namespaceName: "go",
-			quotaName: strings.Join([]string{"wait on ", namespaceName}, ""), csrName: strings.Join([]string{"wait on ", quotaName}, ""),
-			roleName: strings.Join([]string{"wait on ", csrName}, ""), roleBindingName: strings.Join([]string{"wait on ", roleName}, "")}})
+		// Add App Order and App Dependency
+		appOrder, err := json.Marshal(map[string][]string{"apporder": []string{APP}})
+		if err != nil {
+			return pkgerrors.Wrap(err, "Error creating resource order JSON")
+		}
+		appDependency, err := json.Marshal(map[string]map[string]string{"appdependency": map[string]string{APP: "go"}})
 
 		if err != nil {
 			return pkgerrors.Wrap(err, "Error creating resource dependency JSON")
@@ -417,8 +430,118 @@ func CreateEtcdContext(logicalcloud LogicalCloud, clusterList []Cluster,
 			return pkgerrors.Wrap(err, "Error adding instruction dependency to AppContext")
 		}
 
+		// Add App-level Order and Dependency
+		_, err = context.AddInstruction(handle, "app", "order", string(appOrder))
+		_, err = context.AddInstruction(handle, "app", "dependency", string(appDependency))
+
+		// save the context in the cluster db record
+		// FIXME(igordc): code below only works if there's only 1 cluster.
+		// This will be a bit of a problem, so:
+		// - are appcontexts tied to individual clusters?
+		// - should I create one appcontext per cluster in the apply?
+		// - should some rework be done somewhere to allow appcontext per logical cloud?
+		// need feedback, thanks
+		key := clusterPkg.ClusterKey{
+			ClusterProviderName: cluster.Specification.ClusterProvider,
+			ClusterName:         cluster.Specification.ClusterName,
+		}
+		// TODO(igordc): yeah a logical cloud context in the db is probably the right thing
+		err = db.DBconn.Insert("cluster", key, nil, "clustercontext", ctxVal)
+		if err != nil {
+			cleanuperr := context.DeleteCompositeApp()
+			if cleanuperr != nil {
+				log.Warn("Error cleaning AppContext after DB insert failure", log.Fields{
+					"cluster-provider": cluster.Specification.ClusterProvider,
+					"cluster":          cluster.Specification.ClusterName,
+				})
+			}
+			return pkgerrors.Wrap(err, "Error adding AppContext to DB")
+		}
+	}
+
+	fmt.Printf("\nAppContext: %v\n\n", context)
+
+	// call resource synchronizer to instantiate the CRs in the cluster
+	err = installappclient.InvokeInstallApp(ctxVal.(string))
+	if err != nil {
+		return err
 	}
 
 	return nil
 
+}
+
+// DestroyEtcdContext remove from rsync then delete appcontext and all resources
+func DestroyEtcdContext(logicalcloud LogicalCloud, clusterList []Cluster,
+	quotaList []Quota) error {
+
+	// APP := "logical-cloud"
+	// logicalCloudName := logicalcloud.MetaData.LogicalCloudName
+
+	//Resource Names
+	// namespaceName := strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "+namespace"}, "")
+	// roleName := strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "+role"}, "")
+	// roleBindingName := strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "+roleBinding"}, "")
+	// quotaName := strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "+quota"}, "")
+	// csrName := strings.Join([]string{logicalcloud.MetaData.LogicalCloudName, "+CertificateSigningRequest"}, "")
+
+	// context := appcontext.AppContext{}
+	// ctxVal, err := context.InitAppContext()
+	// if err != nil {
+	// 	return pkgerrors.Wrap(err, "Error creating AppContext")
+	// }
+
+	// handle, err := context.CreateCompositeApp()
+	// if err != nil {
+	// 	return pkgerrors.Wrap(err, "Error creating AppContext CompositeApp")
+	// }
+
+	// appHandle, err := context.AddApp(handle, APP)
+	// if err != nil {
+	// 	cleanuperr := context.DeleteCompositeApp()
+	// 	if cleanuperr != nil {
+	// 		log.Warn("Error cleaning AppContext CompositeApp create failure", log.Fields{
+	// 			"logical-cloud": logicalCloudName,
+	// 		})
+	// 	}
+	// 	return pkgerrors.Wrap(err, "Error adding App to AppContext")
+	// }
+
+	// Iterate through cluster list and add all the clusters
+	for _, cluster := range clusterList {
+		clusterProviderName := cluster.Specification.ClusterProvider
+		clusterName := cluster.Specification.ClusterName
+
+		context, ctxVal, err := clusterPkg.NewClusterClient().GetClusterContext(clusterProviderName, clusterName)
+		if err != nil {
+			return pkgerrors.Wrapf(err, "Error finding AppContext for cluster: %v, %v", clusterProviderName, clusterName)
+		}
+
+		// call resource synchronizer to delete the CRs from the cluster
+		err = installappclient.InvokeUninstallApp(ctxVal)
+		if err != nil {
+			return err
+		}
+
+		// remove the app context
+		err = context.DeleteCompositeApp()
+		if err != nil {
+			return pkgerrors.Wrap(err, "Error deleting AppContext CompositeApp")
+		}
+
+		// remove the app context field from the cluster db record
+		key := clusterPkg.ClusterKey{
+			ClusterProviderName: clusterProviderName,
+			ClusterName:         clusterName,
+		}
+		err = db.DBconn.RemoveTag("cluster", key, "clustercontext")
+		if err != nil {
+			log.Warn("Error removing AppContext from Cluster document", log.Fields{
+				"cluster-provider": clusterProviderName,
+				"cluster":          clusterName,
+			})
+		}
+	}
+
+	return nil
 }

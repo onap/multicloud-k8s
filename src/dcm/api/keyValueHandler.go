0054a3e893c124c204d1540bcd1e5c9168eb1f91
Igor D.C <igor.duarte.cardoso@intel.com>
2020-07-29 01:37:15 +0000

    DCM - fix code formatting via vscode (II)
    Part two of fixing code format/syntax with Visual Studio Code.
    
    Furthermore, also switched line endings from CRLF to LF (all test
    files seemed to be ending this way, unlike the other Go files).
    
    Issue-ID: MULTICLOUD-1143
    Signed-off-by: Igor D.C <igor.duarte.cardoso@intel.com>
    Change-Id: Iaae868c780a42ea82ca1208e3cbdc3ec6ffd1f97

--- src/dcm/api/keyValueHandler.go
+++ src/dcm/api/keyValueHandler.go
@@ -14,148 +14,148 @@
 * See the License for the specific language governing permissions
 * and
 * limitations under the License.
-*/
+ */
 
 package api
 
 import (
-    "encoding/json"
-    "net/http"
-    "io"
-    "github.com/onap/multicloud-k8s/src/dcm/pkg/module"
-    "github.com/gorilla/mux"
-)
+	"encoding/json"
+	"io"
+	"net/http"
 
+	"github.com/gorilla/mux"
+	"github.com/onap/multicloud-k8s/src/dcm/pkg/module"
+)
 
 // keyValueHandler is used to store backend implementations objects
 type keyValueHandler struct {
-    client module.KeyValueManager
+	client module.KeyValueManager
 }
 
 // CreateHandler handles creation of the key value entry in the database
 
 func (h keyValueHandler) createHandler(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    project := vars["project-name"]
-    logicalCloud := vars["logical-cloud-name"]
-    var v module.KeyValue
+	vars := mux.Vars(r)
+	project := vars["project-name"]
+	logicalCloud := vars["logical-cloud-name"]
+	var v module.KeyValue
 
-    err := json.NewDecoder(r.Body).Decode(&v)
-    switch {
-    case err == io.EOF:
-        http.Error(w, "Empty body", http.StatusBadRequest)
-        return
-    case err != nil:
-        http.Error(w, err.Error(), http.StatusUnprocessableEntity)
-        return
-    }
+	err := json.NewDecoder(r.Body).Decode(&v)
+	switch {
+	case err == io.EOF:
+		http.Error(w, "Empty body", http.StatusBadRequest)
+		return
+	case err != nil:
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+	}
 
-    // Key Value Name is required.
-    if v.MetaData.KeyValueName == "" {
-        http.Error(w, "Missing name in POST request", http.StatusBadRequest)
-        return
-    }
+	// Key Value Name is required.
+	if v.MetaData.KeyValueName == "" {
+		http.Error(w, "Missing name in POST request", http.StatusBadRequest)
+		return
+	}
 
-    ret, err := h.client.CreateKVPair(project, logicalCloud, v)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	ret, err := h.client.CreateKVPair(project, logicalCloud, v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusCreated)
-    err = json.NewEncoder(w).Encode(ret)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	err = json.NewEncoder(w).Encode(ret)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 // getHandler handle GET operations on a particular name
 // Returns a Key Value
 func (h keyValueHandler) getHandler(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    project := vars["project-name"]
-    logicalCloud := vars["logical-cloud-name"]
-    name := vars["kv-pair-name"]
-    var ret interface{}
-    var err error
+	vars := mux.Vars(r)
+	project := vars["project-name"]
+	logicalCloud := vars["logical-cloud-name"]
+	name := vars["kv-pair-name"]
+	var ret interface{}
+	var err error
 
-    if len(name) == 0 {
-        ret, err = h.client.GetAllKVPairs(project, logicalCloud)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-    } else {
-        ret, err = h.client.GetKVPair(project, logicalCloud, name)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-    }
+	if len(name) == 0 {
+		ret, err = h.client.GetAllKVPairs(project, logicalCloud)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else {
+		ret, err = h.client.GetKVPair(project, logicalCloud, name)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    err = json.NewEncoder(w).Encode(ret)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(w).Encode(ret)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 // UpdateHandler handles Update operations on a particular Key Value
 func (h keyValueHandler) updateHandler(w http.ResponseWriter, r *http.Request) {
-    var v module.KeyValue
-    vars := mux.Vars(r)
-    project := vars["project-name"]
-    logicalCloud := vars["logical-cloud-name"]
-    name := vars["kv-pair-name"]
+	var v module.KeyValue
+	vars := mux.Vars(r)
+	project := vars["project-name"]
+	logicalCloud := vars["logical-cloud-name"]
+	name := vars["kv-pair-name"]
 
-    err := json.NewDecoder(r.Body).Decode(&v)
-    switch {
-        case err == io.EOF:
-            http.Error(w, "Empty body", http.StatusBadRequest)
-            return
-        case err != nil:
-            http.Error(w, err.Error(), http.StatusUnprocessableEntity)
-            return
-    }
+	err := json.NewDecoder(r.Body).Decode(&v)
+	switch {
+	case err == io.EOF:
+		http.Error(w, "Empty body", http.StatusBadRequest)
+		return
+	case err != nil:
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+	}
 
-    // Name is required.
-    if v.MetaData.KeyValueName == "" {
-        http.Error(w, "Missing name in PUT request", http.StatusBadRequest)
-        return
-    }
+	// Name is required.
+	if v.MetaData.KeyValueName == "" {
+		http.Error(w, "Missing name in PUT request", http.StatusBadRequest)
+		return
+	}
 
-    ret, err := h.client.UpdateKVPair(project, logicalCloud, name, v)
-    if err != nil {
-        http.Error(w, err.Error(),
-        http.StatusInternalServerError)
-        return
-    }
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusCreated)
-    err = json.NewEncoder(w).Encode(ret)
-    if err != nil {
-        http.Error(w, err.Error(),
-        http.StatusInternalServerError)
-        return
-    }
+	ret, err := h.client.UpdateKVPair(project, logicalCloud, name, v)
+	if err != nil {
+		http.Error(w, err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	err = json.NewEncoder(w).Encode(ret)
+	if err != nil {
+		http.Error(w, err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
 }
 
 //deleteHandler handles DELETE operations on a particular record
 func (h keyValueHandler) deleteHandler(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    project := vars["project-name"]
-    logicalCloud := vars["logical-cloud-name"]
-    name := vars["kv-pair-name"]
+	vars := mux.Vars(r)
+	project := vars["project-name"]
+	logicalCloud := vars["logical-cloud-name"]
+	name := vars["kv-pair-name"]
 
-        err := h.client.DeleteKVPair(project, logicalCloud, name)
-        if err != nil {
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-                return
-        }
+	err := h.client.DeleteKVPair(project, logicalCloud, name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-        w.WriteHeader(http.StatusNoContent)
+	w.WriteHeader(http.StatusNoContent)
 }

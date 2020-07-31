66e8600ccbbb96cbf22397cb10af02aca03868a4
Eric Multanen <eric.w.multanen@intel.com>
2020-07-30 19:27:54 -0700

    Add appcontext state, status and resource status
    Add support in the AppContext for managing an AppContext
    (composite app level) state and status value.
    Also adds support for tracking rsync status at the resource
    level.
    
    Issue-ID: MULTICLOUD-1042
    Signed-off-by: Eric Multanen <eric.w.multanen@intel.com>
    Change-Id: I3d0a9a97ea45ca11f9f873104476e4b67521e56a

---
+++ kud/tests/gctxt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Simple script to view appcontext
+# with no argumnet, it will list all keys under /context/
+# with 1 argument, it will show the value of the key provided
+# note: assumes emoco services are running in namespace emco
+if [ "$#" -ne 1 ] ; then
+    kubectl -n emco exec `kubectl get pods -lapp=etcd -n emco --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}'` -it -- etcdctl get /context/ --prefix=true --keys-only=true
+else
+    kubectl -n emco exec `kubectl get pods -lapp=etcd -n emco --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}'` -it -- etcdctl get $1 --prefix=true
+fi

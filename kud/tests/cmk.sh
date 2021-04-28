42aaa759da06b08560d22892879e1004e5b9344d
Todd Malsbary <todd.malsbary@intel.com>
2021-04-28 12:51:42 -0700

    Exit cmk test script on error
    Issue-ID: MULTICLOUD-1335
    Signed-off-by: Todd Malsbary <todd.malsbary@intel.com>
    Change-Id: I83336983475bab25867c44153657d31b1bacb933

--- kud/tests/cmk.sh
+++ kud/tests/cmk.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+source _common.sh
+
 ENV=$(kubectl get nodes --all-namespaces | wc -l)
 if [[ $ENV -gt 2 ]]; then
     COMPUTE_NODE=$(kubectl get nodes --all-namespaces | grep -v master | awk 'NR==2{print $1}')
@@ -59,8 +62,8 @@ for ((i=0;i<$num;i++)); do
     echo "##################################"
     if [ "${case[$POOL]}" == "exclusive" ]; then
         echo "TC: to allocate ${case[$CORE]} CPU(s) from pool of ${case[$POOL]} on node of ${case[$NODE]}"
-        TOTAL=$(kubectl get cmk-nodereport ${case[$NODE]} -o json | jq .spec.report.description.pools.${case[$POOL]} | jq .cpuLists | awk -F  '{' '{print $(NF)}' | awk -F  '}' '{print $(NF)}' | awk -F  ',' '{print $(NF)}' | grep "\"tasks\": \[" | wc -l)
-            echo "ready to generate yaml"
+        TOTAL=$(kubectl get cmk-nodereport ${case[$NODE]} -o json | jq --arg pool "${case[$POOL]}" '.spec.report.description.pools[$pool].cpuLists|length')
+        echo "ready to generate yaml"
 cat << EOF > $DIR/$pod_name.yaml
     apiVersion: v1
     kind: Pod
@@ -113,8 +116,11 @@ EOF
         done
         echo "waiting for CPU allocation finished ..."
         rest=$TOTAL
-        until [[ $TOTAL -gt $rest ]]; do
-            rest=$(kubectl get cmk-nodereport ${case[$NODE]} -o json | jq .spec.report.description.pools.exclusive | jq .cpuLists | awk -F  '{' '{print $(NF)}' | awk -F  '}' '{print $(NF)}' | awk -F  ',' '{print $(NF)}' | grep "\"tasks\": \[\]" | wc -l)
+        timeout=0
+        until [[ $TOTAL -gt $rest || $timeout == 180 ]]; do
+            sleep 1
+            rest=$(kubectl get cmk-nodereport ${case[$NODE]} -o json | jq '.spec.report.description.pools.exclusive.cpuLists[].tasks|select(type=="array" and length==0)' | wc -l)
+            timeout=$((timeout+1))
         done
         let allocated=`expr $TOTAL - $rest`
         echo "The allocated CPU amount is:" $allocated
@@ -123,7 +129,7 @@ EOF
         if [[ $allocated == ${case[$CORE]} ]]; then
             echo "CPU was allocated as expected, TC passed !!"
         else
-            echo "failed to allocate CPU, TC failed !!"
+            echo "failed to allocate CPU ($allocated != ${case[$CORE]}), TC failed !!"
         fi
         rm -f $DIR/$pod_name.yaml
         echo "ready to delete pod"
@@ -132,6 +138,7 @@ EOF
         echo "##################################"
         echo
         echo
+        [[ $allocated == ${case[$CORE]} ]]
     else
         echo "TC: to allocate CPU(s) from pool of ${case[$POOL]} on node of ${case[$NODE]}"
         echo "ready to generate yaml"
@@ -207,10 +214,10 @@ EOF
         echo "waiting for CPU allocation finished ..."
         rest=0
         timeout=0
-        until [ $rest == 2 -o $timeout == 180 ]; do
-            rest=$(kubectl get cmk-nodereport ${case[$NODE]} -o json | jq .spec.report.description.pools.shared | jq .cpuLists | awk -F  '{' '{print $(NF)}' | awk -F  '}' '{print $(NF)}' | grep -v "cpus" | grep "  "| grep -v "tasks"| grep -v "\]" | wc -l)
-            sleep -- 1
-            let timeout++
+        until [[ $rest == 2 || $timeout == 180 ]]; do
+            sleep 1
+            rest=$(kubectl get cmk-nodereport ${case[$NODE]} -o json | jq '.spec.report.description.pools.shared.cpuLists[].tasks' | jq length)
+            timeout=$((timeout+1))
         done
         echo "The CPU allocated in shared pool for 2 tasks"
         echo "deploy a nginx pod"
@@ -218,7 +225,7 @@ EOF
         if [[ $rest == 2 ]]; then
             echo "CPU was allocated as expected, TC passed !!"
         else
-            echo "failed to allocate CPU, TC failed !!"
+            echo "failed to allocate CPU ($rest != 2), TC failed !!"
         fi
         rm -f $DIR/$pod_name.yaml
         echo "ready to delete pod"
@@ -227,5 +234,6 @@ EOF
         echo "##################################"
         echo
         echo
+        [[ $rest == 2 ]]
     fi
 done

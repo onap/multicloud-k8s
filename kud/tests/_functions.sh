bf42f3a5815e2c4c00adfe5d1aa4f325559ecd3e
Fiete Ostkamp <fiete.ostkamp@telekom.de>
2025-10-23 11:43:32 +0200

    Improve docker compose setup
    - add start_period and check intervals to healthcheck in compose
    - use docker network instead of host network [0]
    - log error message when reading the config file fails
    
    [0] this allows using the container names as service names and thus
        avoids the need to determine the dynamically allocated container ips
        on the host.
    
    Issue-ID: MULTICLOUD-1522
    Change-Id: I3ca1f4c33accba5a5b6329e7e84343fc4b6ac055
    Signed-off-by: Fiete Ostkamp <fiete.ostkamp@telekom.de>

--- kud/tests/_functions.sh
+++ kud/tests/_functions.sh
@@ -91,11 +91,16 @@ function call_api {
         return 2
     else
         echo "[INFO] Server replied with status: ${status}" >&2
-        cat "${curl_response_file}"
-        rm "${curl_response_file}"
         if [[ "${status:0:1}" =~ [45] ]]; then
+            echo $(cat "${curl_response_file}") >&2
+            echo $(docker compose logs -f $WORKSPACE/deployments/docker-compose.yml --tail=30 multicloud-k8s) >&2
+            # `cat` determines the return value on stdout for the caller here
+            cat "${curl_response_file}"
+            rm "${curl_response_file}"
             return 1
         else
+            cat "${curl_response_file}"
+            rm "${curl_response_file}"
             return 0
         fi
     fi
